710066e586a4fc35308ae7b88697402787f54af5
avictor94 <hardtonameit@naver.com>
2017-04-07 11:51:31 +0900

    Calendar Beans

---
+++ SMTMProject/src/controller/GetCalendarListController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import model.CalendarBean;
+import model.CalendarManager;
+
+public class GetCalendarListController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		int year = Integer.parseInt(request.getParameter("year"));
+		int month = Integer.parseInt(request.getParameter("month"));
+		CalendarManager cm = CalendarManager.getInstance();
+		cm.setCurrent(year, month);
+		CalendarBean cb = cm.getCurrent();
+		
+		JSONObject cbObj = new JSONObject(cb);
+		out.print(cbObj);
+		return null;
+	}
+}
