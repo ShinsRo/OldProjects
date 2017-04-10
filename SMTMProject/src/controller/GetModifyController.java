eaa773dcd4393527691877f562fd103dab474219
sy2399 <ter194@ajou.ac.kr>
2017-04-10 17:35:04 +0900

    sessiont수정

--- SMTMProject/src/controller/GetModifyController.java
+++ SMTMProject/src/controller/GetModifyController.java
@@ -18,11 +18,14 @@ public class GetModifyController implements Controller {
       String id = request.getParameter("id");
       String name=request.getParameter("name");
       int limit=Integer.parseInt(request.getParameter("limit"));
+     
       MemberVO vo=new MemberVO();
       vo.setPassword(password);
       vo.setId(id);
       vo.setName(name);
       vo.setLimit(limit);
+      
+      session.setAttribute("mvo", vo);
       MemberDAO.getInstance().updateMember(vo);
       return "/member/modify_result.jsp";
    }
