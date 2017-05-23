8eee89e01a8430dc6563fec919526d0378b1fa1e
[garin0] <[garin0112@naver.com]>
2017-05-23 17:23:04 +0900

    mybatis member 수정

--- src/main/java/org/kosta/goodmove/model/dao/MemberDAO.java
+++ src/main/java/org/kosta/goodmove/model/dao/MemberDAO.java
@@ -1,5 +1,9 @@
 package org.kosta.goodmove.model.dao;
 
+import org.kosta.goodmove.model.vo.MemberVO;
+
 public interface MemberDAO {
+
+	MemberVO findByIdMember(String id);
 
 }
--- src/main/java/org/kosta/goodmove/model/dao/MemberDAOImpl.java
+++ src/main/java/org/kosta/goodmove/model/dao/MemberDAOImpl.java
@@ -2,6 +2,7 @@ package org.kosta.goodmove.model.dao;
 
 import javax.annotation.Resource;
 
+import org.kosta.goodmove.model.vo.MemberVO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,9 @@ import org.springframework.stereotype.Repository;
 public class MemberDAOImpl implements MemberDAO{
 	@Resource
 	 private SqlSessionTemplate template;
+	
+ 	@Override
+ 	public MemberVO findByIdMember(String id){
+ 		return template.selectOne("member.findMemberById", id);
+ 	}
 }
