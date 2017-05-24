4344b5fe4ddf21c95cd5bd0ff0d0109f946e40e1
[garin0] <[garin0112@naver.com]>
2017-05-24 09:39:09 +0900

    init

--- src/main/java/org/kosta/goodmove/model/dao/MemberDAO.java
+++ src/main/java/org/kosta/goodmove/model/dao/MemberDAO.java
@@ -1,9 +1,6 @@
 package org.kosta.goodmove.model.dao;
 
-import org.kosta.goodmove.model.vo.MemberVO;
-
 public interface MemberDAO {
 
-	MemberVO findByIdMember(String id);
 
 }
--- src/main/java/org/kosta/goodmove/model/dao/MemberDAOImpl.java
+++ src/main/java/org/kosta/goodmove/model/dao/MemberDAOImpl.java
@@ -2,7 +2,6 @@ package org.kosta.goodmove.model.dao;
 
 import javax.annotation.Resource;
 
-import org.kosta.goodmove.model.vo.MemberVO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +9,4 @@ import org.springframework.stereotype.Repository;
 public class MemberDAOImpl implements MemberDAO{
 	@Resource
 	 private SqlSessionTemplate template;
-	
- 	@Override
- 	public MemberVO findByIdMember(String id){
- 		return template.selectOne("member.findMemberById", id);
- 	}
 }
