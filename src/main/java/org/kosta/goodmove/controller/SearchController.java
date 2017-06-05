39d7237ca9d64f4553e12fbb310808dae7d063ee
JinwooLim <gkdlqhdl2000@nate.com>
2017-06-05 15:14:10 +0900

    admin main, 방문자 받아오는 메서드 수정

--- src/main/java/org/kosta/goodmove/controller/SearchController.java
+++ src/main/java/org/kosta/goodmove/controller/SearchController.java
@@ -1,11 +1,16 @@
 package org.kosta.goodmove.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.kosta.goodmove.model.service.BoardService;
 import org.kosta.goodmove.model.service.CommentService;
 import org.kosta.goodmove.model.service.MemberService;
 import org.kosta.goodmove.model.service.SearchService;
+import org.kosta.goodmove.model.vo.MemberVO;
 import org.kosta.goodmove.model.vo.SearchVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,39 +28,77 @@ public class SearchController {
 	private CommentService commentService;
 	@Resource
 	private MemberService memberService;
-	
+
 	/**
 	 * 메인화면에 표시되는 기부물품 글갯수
+	 * 
 	 * @return
 	 */
 	@RequestMapping("getCountBoardMain.do")
 	@ResponseBody
-	public int getCountBoardMain(){
+	public int getCountBoardMain() {
 		return boardService.getCountBoard();
 	}
-	
+
 	/**
 	 * 메인화면에 표시되는 지역후기 글갯수
+	 * 
 	 * @return
 	 */
 	@RequestMapping("getCountCommentMain.do")
 	@ResponseBody
-	public int getCountCommentMain(){
+	public int getCountCommentMain() {
 		return commentService.getTotalContentCount();
 	}
-	
+
 	/**
 	 * 메인화면에 표시되는 회원수
+	 * 
 	 * @return
 	 */
 	@RequestMapping("getCountMemberMain.do")
 	@ResponseBody
-	public int getCountMemberMain(){
+	public int getCountMemberMain() {
 		return memberService.getMemberCount();
 	}
-	
+
+	/**
+	 * 메인화면에 표시되는 오늘의 방문자
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("getCountToDay.do")
+	@ResponseBody
+	public int getCountToDay(HttpServletRequest request) {
+		MemberVO mvo = (MemberVO) request.getSession().getAttribute("mvo");
+		String info = null;
+		if (mvo != null) {
+			try {
+				int i = InetAddress.getLocalHost().toString().lastIndexOf("/");
+				info = mvo.getId() + InetAddress.getLocalHost().toString().substring(i);
+				
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				int i = InetAddress.getLocalHost().toString().lastIndexOf("/");
+				info = InetAddress.getLocalHost().toString().substring(i);
+				;
+
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return searchService.countday(info);
+	}
+
 	/**
 	 * 검색기능
+	 * 
 	 * @param vo
 	 * @param pageNo
 	 * @param model
@@ -85,14 +128,13 @@ public class SearchController {
 		model.addAttribute("cid", searchService.count(new SearchVO("comment", "id", vo.getWord())));
 		model.addAttribute("svo", vo);
 		String type;
-		if(vo.getMcategory().equals("board")){
-			type="b"+vo.getScategory();
-		}else{
-			type="c"+vo.getScategory();
+		if (vo.getMcategory().equals("board")) {
+			type = "b" + vo.getScategory();
+		} else {
+			type = "c" + vo.getScategory();
 		}
 		model.addAttribute("type", type);
 		model.addAttribute("mcategory", vo.getMcategory());
 		return "search/searchResult.tiles";
 	}
 }
-
