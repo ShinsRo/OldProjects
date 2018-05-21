9d29f685e4ea592c7e32a50022432e15ea8cc01f
siotman0728 <siotman0728@gmail.com>
2018-05-21 22:03:22 +0900

    access-control-allow-origin prob. solv. for front Dev-server

--- src/main/java/com/midas2018/root/controller/DemoUserController.java
+++ src/main/java/com/midas2018/root/controller/DemoUserController.java
@@ -3,6 +3,7 @@ package com.midas2018.root.controller;
 import com.midas2018.root.model.DemoUserVO;
 import com.midas2018.root.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,8 @@ public class DemoUserController {
     @Autowired
     private DemoService demoService;
 
+    /* CrossOrigin : front-end devServer를 위한 크로스 오리진 허용 설정*/
+    @CrossOrigin
     @GetMapping(value = "/findUserById.do")
     public DemoUserVO findUserById(){
         System.out.println("findUserById");
