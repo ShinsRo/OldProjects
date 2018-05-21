9320dc7aabcb66d958f6982ff3de65f78a77f0d4
siotman0728 <siotman0728@gmail.com>
2018-05-21 22:31:29 +0900

    Lombok implementation

--- src/main/java/com/midas2018/root/model/DemoUserVO.java
+++ src/main/java/com/midas2018/root/model/DemoUserVO.java
@@ -1,48 +1,12 @@
 package com.midas2018.root.model;
 
+import lombok.Data;
+
+@Data
 public class DemoUserVO {
     private String id;
     private String password;
     private String data;
-
-    public DemoUserVO(String id, String password, String data) {
-        this.id = id;
-        this.password = password;
-        this.data = data;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                "id:'" + id + '\'' +
-                ", password:'" + password + '\'' +
-                ", data:'" + data + '\'' +
-                '}';
-    }
 }
 
 
