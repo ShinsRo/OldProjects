202f0e5d652affef703515406ff8752fac1025a1
woojin.kang <woojin.kang@linecorp.com>
2018-05-22 18:40:23 +0900

    Add http-logger

--- src/main/java/com/midas2018/root/RootApplication.java
+++ src/main/java/com/midas2018/root/RootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RootApplication {
+public class RootApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(RootApplication.class, args);
