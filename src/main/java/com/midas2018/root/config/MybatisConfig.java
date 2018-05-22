bfed964ff3fdbd19ee9019196f438215d8b02bbc
siotman0728 <siotman0728@gmail.com>
2018-05-23 08:03:22 +0900

    memberMapper path registered

--- src/main/java/com/midas2018/root/config/MybatisConfig.java
+++ src/main/java/com/midas2018/root/config/MybatisConfig.java
@@ -21,6 +21,7 @@ public class MybatisConfig {
         sqlSessionFactoryBean.setConfiguration(configuration);
         sqlSessionFactoryBean.setMapperLocations(new Resource[] {
                 new ClassPathResource("mapper/DemoUserMapper.xml"),
+                new ClassPathResource("mapper/MemberMapper.xml")
                 });
         return sqlSessionFactoryBean.getObject();
     }
