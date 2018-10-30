eb36e334ec9ccfb016f91512fed1f574d3224e14
siotman <siotman0728@gmail.com>
2018-10-31 07:15:46 +0900

    temp

--- src/modules/models.py
+++ src/modules/models.py
@@ -1,10 +1,9 @@
-class PaperEntity:
-    def __init__(self, doi, title, cited, issn, journal, author, date):
-        self.doi = doi
-        self.title = title
-        self.cited = cited
-        self.issn = issn
-        self.journal = journal
-        self.author = author
-        self.date = date
-
+# class PaperEntity:
+#     def __init__(self, doi, title, cited, issn, journal, author, date):
+#         self.doi = doi
+#         self.title = title
+#         self.cited = cited
+#         self.issn = issn
+#         self.journal = journal
+#         self.author = author
+#         self.date = date
