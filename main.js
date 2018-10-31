5dbe85dda953abddbc550949f9d554ccc72e189f
siotman <siotman0728@gmail.com>
2018-10-31 09:02:53 +0900

    Add gui base

---
+++ main.js
@@ -0,0 +1,14 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow () {
+    window = new BrowserWindow({width: 800, height: 600})
+    window.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+})
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import sys
 from src.modules.scraper import Scraper
 
-sp = Scraper()
+results = []
+
+# sample
+########################################
+results = {}
+for idx, arg in enumerate(sys.argv[1:]):
+    results[idx] = "result for %s"%arg
+########################################
+
+print(results)
