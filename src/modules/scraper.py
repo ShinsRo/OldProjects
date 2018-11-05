65cd0a2ea9e81b56e7541a706a9e6b9f5645902e
siotman <siotman0728@gmail.com>
2018-11-05 11:41:39 +0900

    코드 패키징

--- src/modules/scraper.py
+++ src/modules/scraper.py
@@ -1,5 +1,6 @@
 from robobrowser import RoboBrowser
 from bs4 import BeautifulSoup
+from multiprocessing import Process
 import urllib
 import json
 import re 
@@ -13,18 +14,23 @@ class Scraper():
         self.paperList = []
         self.baseUrl = "http://apps.webofknowledge.com"
         self.state = "waiting"
-        self.browser = RoboBrowser(history=True, parser="lxml")
+        self.logState(state="0000", stateMSG="SID와 jsessionid 값을 얻습니다.")
 
-        self.logState(state="0000", stateMSG="getting SID")
+
+        self.browser = RoboBrowser(history=True, parser="lxml")
+        # 테스트용
+        self.SID = "E2V7RPJe7OYXtfigmbk"
+        self.jsessionid = "506EB868497C83AB26B65AA714B04878"
+
         self.browser.open(self.baseUrl)
+
         self.SID = self.browser.session.cookies['SID'].replace("\"", "")
         self.jsessionid = self.browser.session.cookies['JSESSIONID']
 
         self.logState(state="0001", stateMSG="SID : %s"%self.SID)
         self.logState(state="0002", stateMSG="jsessionid : %s"%self.jsessionid)
-
-
         self.logState(state="0003", stateMSG="작업을 위해 WOS 페이지에 접근 중입니다.")
+
 
         actionURL = self.baseUrl
         action = "/WOS_AdvancedSearch_input.do"
@@ -37,12 +43,10 @@ class Scraper():
         actionURL += param
 
         self.browser.open(actionURL)
+        self.WOS_AdvancedSearch_input_form = self.browser.get_form(id="WOS_AdvancedSearch_input_form")
 
         self.logState(state="0200", stateMSG="질의를 시작할 수 있습니다.")
 
-        # # 테스트용
-        # self.SID = "E6iQIWCw3PkqehegemT"
-        # self.jsessionid = "F1712DE4685F64BD8AC425E3B91FF108"
 
     def makeQueryFromFile(self, path, packSize, gubun):
         # if csv
@@ -59,202 +63,179 @@ class Scraper():
 
         return (queryList, words)
 
-    def queryProcess(self, browser, words):
-        search_form = browser.get_form(id="WOS_AdvancedSearch_input_form")
-        search_form['value(input1)'] = query
+    # def queryProcess(self, browser, words):
+    #     search_form = browser.get_form(id="WOS_AdvancedSearch_input_form")
+    #     search_form['value(input1)'] = query
 
-        browser.submit_form(search_form)
-        historyResults = browser.select('div.historyResults')
+    #     browser.submit_form(search_form)
+    #     historyResults = browser.select('div.historyResults')
         
-        for idx, his in enumerate(historyResults):
-            rsCnt = his.a.text
-            rsCnt = rsCnt.replace(",","")
-            rsCnt = int(rsCnt)
+    #     for idx, his in enumerate(historyResults):
+    #         rsCnt = his.a.text
+    #         rsCnt = rsCnt.replace(",","")
+    #         rsCnt = int(rsCnt)
 
-            if rsCnt >= 10000:
-                self.logState(state="1150", stateMSG="결과가 10000개를 넘을 수 없으므로, 쿼리를 분할합니다.")
-                wordsLen = len(words)
-                lHisRs = queryProcess(browser, words[:wordsLen//2 + 1])
-                rHisRs = queryProcess(browser, words[wordsLen//2 + 1:])
+    #         if rsCnt >= 10000:
+    #             self.logState(state="1150", stateMSG="결과가 10000개를 넘을 수 없으므로, 쿼리를 분할합니다.")
+    #             wordsLen = len(words)
+    #             lHisRs = self.queryProcess(browser, words[:wordsLen//2 + 1])
+    #             rHisRs = self.queryProcess(browser, words[wordsLen//2 + 1:])
                 
-                historyResults = lHisRs + rHisRs
+    #             historyResults = lHisRs + rHisRs
 
-        return historyResults
-
-    def exportExcel(self, data, processId, outputLocationPath):
-        with open(processId + "_excel_rs.xls", "wb") as rsFile:
-                rsFile.write(data)
+    #     return historyResults
 
     def logState(self, state, stateMSG):
         self.state = state
         self.stateMSG = stateMSG
         print('[state %s] %s'%(self.state, self.stateMSG))
 
-    # Run method
-    ####################################
-    def run(self, startDate, endDate, gubun, inputFilePath, outputLocationPath, defaultQueryPackSize):
-        # 테스트 데이터
-        # startDate = "201001"
-        # endDate = "201001"
-        # gubun = "TI" 
-        # inputFilePath = "C:\\Users\\F\\Desktop\\papers\\sju-paper-scraper-app\\src\\modules\\"
-        # outputLocationPath="C:\\Users\\F\\Desktop\\papers\\sju-paper-scraper-app\\src\\modules\\"
+    def getWOSExcel(self, idx, his, mark_from, mark_to, totalMarked):
+        browser = RoboBrowser(history=True, parser="lxml")
+        browser.open(self.baseUrl + his.a["href"])
+        reportLink = browser.select("a.citation-report-summary-link")[0]
 
-        # 단계 1/6 :
-        ##############################################
-        self.logState(state="1001", stateMSG="WOS AdvancedSearch를 시작합니다.")
+        browser.follow_link(reportLink)
+        
+        summary_records_form = browser.get_form(id='summary_records_form')
+        CRTitle = browser.select("div.CRnewpageTitle")[0]
+        summary_records_form_data = {
+            "qid" : summary_records_form['qid'].value,
+            "filters" : summary_records_form['filters'].value,
+            "sortBy" : summary_records_form['sortBy'].value,
+            "timeSpan" : summary_records_form['timeSpan'].value,
+            "endYear" : summary_records_form['endYear'].value,
+            "startYear" : summary_records_form['startYear'].value,
+            "rurl" : summary_records_form['rurl'].value,
+            # "totalMarked" : CRTitle.span.string.replace(",", ""),
+            "totalMarked" : totalMarked,
+            "piChart" : summary_records_form['piChart'].value,
+            "toChart" : summary_records_form['piChart'].value
+        }
+        formData = {
+            "SID" : self.SID,
+            "additional_qoutput_params": "cr_qid="+summary_records_form_data['qid'],
+            "selectedIds": "",
+            "displayCitedRefs":"",
+            "displayTimesCited":"",
+            "displayUsageInfo":"true",
+            "viewType":"summary",
+            "product":"WOS",
+            "mark_id":"WOS",
+            "colName":"WOS",
+            "search_mode":"CitationReport",
+            "view_name":"WOS-CitationReport-summary",
+            "mode":"OpenOutputService",
+            "format":"crsaveToFile",
+            "queryNatural":"",
+            "count_new_items_marked":"0",
+            "use_two_ets":"false",
+            "IncitesEntitled":"no",
+            "value(record_select_type)":"range",
+            "action":"recalulate",
+            "start_year_val":"1900",
+            "end_year_val":"2019",
+            "viewAbstractUrl":"",
+            "LinksAreAllowedRightClick": "full_record.do",
+            "db_editions": "",
+            "print_opt":"Html",
+            "include_mark_from_in_url":"true",
+            "fields":"DUMMY_VALUE"
+        }
+        formData.update(summary_records_form_data)
+
+        makeExcelURL = "http://apps.webofknowledge.com/OutboundService.do?"
+        makeExcelParam = ""
+        makeExcelParam += "action=go"
+        makeExcelParam += "&save_options=xls"
+
+        makeExcelURL += makeExcelParam
+
+        browser.session.post(
+            makeExcelURL, 
+            data=formData
+        )
+
+        ExcelActionURL = "https://ets.webofknowledge.com/ETS/ets.do?"
+
+        ExcelParam = "product=UA"
+        ExcelParam += "&colName=WOS"
+        ExcelParam += "&displayUsageInfo=true"
+        ExcelParam += "&parentQid=" + formData['qid']
+        ExcelParam += "&rurl=" + requests.utils.quote(formData['rurl'])
+        ExcelParam += "&startYear=" + formData['startYear']
+        ExcelParam += "&filters=" + requests.utils.quote(formData['filters'])
+        ExcelParam += "&qid=" + str(int(formData['qid'])+1)
+        ExcelParam += "&endYear=" + formData['endYear']
+        ExcelParam += "&SID=" + self.SID
+        ExcelParam += "&totalMarked=" + formData['totalMarked']
+        ExcelParam += "&action=crsaveToFile"
+        ExcelParam += "&timeSpan=" + requests.utils.quote(formData['timeSpan'])
+        ExcelParam += "&sortBy=" + formData['sortBy']
+        ExcelParam += "&displayTimesCited=false"
+        ExcelParam += "&displayCitedRefs=true"
+        ExcelParam += "&fileOpt=xls"
+        ExcelParam += "&UserIDForSaveToRID=null"
+        ExcelParam = "&mark_from=" + mark_from
+        ExcelParam += "&mark_to=" + mark_to
+
+        ExcelActionURL += ExcelParam
+
+        res = requests.get(ExcelActionURL)
+        resStr = res.text
+
+        with open(str(idx) + "_excel_rs.xls", "wb") as rsFile:
+            rsFile.write(res.content)
+
+        return resStr
+
+    # Run method
+    def run(self, startDate, endDate, gubun, inputFilePath, outputLocationPath, defaultQueryPackSize):
+        self.logState(
+            state="1001", 
+            stateMSG="WOS AdvancedSearch를 시작합니다.")
         browser = self.browser
 
-        # actionURL = self.baseUrl
-        # action = "/WOS_AdvancedSearch_input.do"
-        # param = ""
-        # param += "?SID=" + self.SID
-        # param += "&product=WOS"
-        # param += "&search_mode=AdvancedSearch"
-        
-        # actionURL += action
-        # actionURL += param
-
-        # browser.open(actionURL)
-
-        self.logState(state="1002", stateMSG="쿼리를 만듭니다.")
-        queryList, words = self.makeQueryFromFile(inputFilePath, defaultQueryPackSize, gubun)
+        self.logState(
+            state="1002", 
+            stateMSG="쿼리를 만듭니다.")
+        # queryList, words = self.makeQueryFromFile(inputFilePath, defaultQueryPackSize, gubun)
+        queryList, words = self.makeQueryFromFile(inputFilePath, 0, gubun)
 
         queryListLen = len(queryList)
         for idx, query in enumerate(queryList):
-            # self.logState(state="1010", stateMSG="Query : %s"%query)
-            self.logState(state="1100", stateMSG="쿼리를 시작합니다. %d/%d"%(idx+1, queryListLen))
-            
-            search_form = browser.get_form(id="WOS_AdvancedSearch_input_form")
-            search_form['value(input1)'] = query
-
-            browser.submit_form(search_form)
+            self.logState(
+                state="1100", 
+                stateMSG="쿼리를 시작합니다. %d/%d"%(idx+1, queryListLen))
+            self.WOS_AdvancedSearch_input_form['value(input1)'] = query
+            browser.submit_form(self.WOS_AdvancedSearch_input_form)
 
         historyResults = browser.select('div.historyResults')
+        historyResultsLen = len(historyResults)  
         
-        historyResultsLen = len(historyResults)        
-        for idx,his in enumerate(historyResults):
+        resStr = ""
+        notFound = list(map(lambda x: [x, False], words))
+
+        for idx, his in enumerate(historyResults):
             self.logState(state="1200", stateMSG="쿼리 팩키지를 실행합니다. %d/%d"%(idx+1, historyResultsLen))
+            totalMarked = his.a.text.replace(",", "")
 
-            browser.follow_link(his.a)
+            for mark in range(1, int(totalMarked), 500):
+                self.logState(
+                    state="0000", 
+                    stateMSG="%s개의 레코드 중 %d부터 레코드를 가져옵니다."%(totalMarked, mark))
+                resStr += self.getWOSExcel(idx, his, str(mark), str(mark + 500 - 1), totalMarked)
+        
+        # words 에 따라 검색해서 없는 결과를 추출해야함.
+        for idx, word in enumerate(notFound):
+            notFound[idx][1] =( -1 != resStr.find(word[0]) )
+        
 
-            reportLink = browser.select("a.citation-report-summary-link")
-            browser.follow_link(reportLink[0])
+        # 세션을 파괴
+        self.browser = RoboBrowser(history=True, parser="lxml")
+        self.browser.open(self.baseUrl)
 
-            summary_records_form = browser.get_form(id='summary_records_form')
-            qid = summary_records_form['qid'].value
-            filters = summary_records_form['filters'].value
-            sortBy = summary_records_form['sortBy'].value
-            timeSpan = summary_records_form['timeSpan'].value
-            endYear = summary_records_form['endYear'].value
-            startYear = summary_records_form['startYear'].value
-            rurl = summary_records_form['rurl'].value
-
-            CRTitle = browser.select("div.CRnewpageTitle")[0]
-            totalMarked = CRTitle.span.string.replace(",", "")
-
-            int(totalMarked) / 500
-
-            mark_to = totalMarked
-            mark_from = "1"
-
-            piChart = summary_records_form['piChart'].value
-            toChart = summary_records_form['piChart'].value
-
-            makeExcelURL = "http://apps.webofknowledge.com/OutboundService.do?"
-            makeExcelParam = ""
-            makeExcelParam += "action=go"
-            makeExcelParam += "&save_options=xls"
-
-            makeExcelURL += makeExcelParam
-
-            browser.session.post(makeExcelURL, data={
-                "selectedIds": "",
-                "displayCitedRefs":"",
-                "displayTimesCited":"",
-                "displayUsageInfo":"true",
-                "viewType":"summary",
-                "product":"WOS",
-                "rurl":rurl,
-                "mark_id":"WOS",
-                "colName":"WOS",
-                "search_mode":"CitationReport",
-                "view_name":"WOS-CitationReport-summary",
-                "sortBy": sortBy,
-                "mode":"OpenOutputService",
-                "qid":qid,
-                "SID":self.SID,
-                "format":"crsaveToFile",
-                "mark_to":mark_to,
-                "mark_from":mark_from,
-                "queryNatural":"",
-                "count_new_items_marked":"0",
-                "use_two_ets":"false",
-                "IncitesEntitled":"no",
-                "value(record_select_type)":"range",
-                "markFrom":mark_from,
-                "markTo":mark_to,
-                "action":"recalulate",
-                "start_year_val":"1900",
-                "end_year_val":"2019",
-                "viewAbstractUrl":"",
-                "LinksAreAllowedRightClick": "full_record.do",
-                "filters":filters,
-                "timeSpan": timeSpan,
-                "db_editions": "",
-                "additional_qoutput_params": "cr_qid="+qid,
-                "print_opt":"Html",
-                "include_mark_from_in_url":"true",
-                "endYear":endYear,
-                "startYear":startYear,
-                "piChart":piChart,
-                "toChart":toChart,
-                "fields":"DUMMY_VALUE"
-            })
-
-
-            ExcelActionURL = "https://ets.webofknowledge.com"
-            ExcelAction = "/ETS/ets.do?"
-
-            # summary_navigation = browser.get_form(id='summary_navigation')
-            # print(summary_navigation.parsed)
-            
-            ExcelParam = "mark_from=1"
-            ExcelParam += "&product=UA"
-            ExcelParam += "&colName=WOS"
-            ExcelParam += "&displayUsageInfo=true"
-            ExcelParam += "&parentQid=" + qid
-            ExcelParam += "&rurl=" + requests.utils.quote(rurl)
-            ExcelParam += "&startYear=" + startYear
-            ExcelParam += "&mark_to=" + mark_to
-            ExcelParam += "&filters=" + requests.utils.quote(filters)
-            ExcelParam += "&qid=" + str(int(qid)+1)
-            ExcelParam += "&endYear=" + endYear
-            ExcelParam += "&SID=" + self.SID
-            ExcelParam += "&totalMarked=" + totalMarked
-            ExcelParam += "&action=crsaveToFile"
-            ExcelParam += "&timeSpan=" + requests.utils.quote(timeSpan)
-            ExcelParam += "&sortBy=" + sortBy
-            ExcelParam += "&displayTimesCited=false"
-            ExcelParam += "&displayCitedRefs=true"
-            ExcelParam += "&fileOpt=xls"
-            ExcelParam += "&UserIDForSaveToRID=null"
-
-            ExcelActionURL += ExcelAction
-            ExcelActionURL += ExcelParam
-
-            res = requests.get(ExcelActionURL)
-            excel = res.content
-            resStr = res.text
-
-            notFound = []
-            for word in words:
-                if resStr.find(word) == -1:
-                    notFound.append(word)
-
-            with open(str(idx) + "_excel_rs.xls", "wb") as rsFile:
-                rsFile.write(res.content)
-
+        return
     ####################################
 
 class PaperEntity:
--- test/test_001.py
+++ test/test_001.py
@@ -5,9 +5,11 @@ srap.run(
     startDate="201801", 
     endDate="201901", 
     gubun="TI", 
-    inputFilePath="C:\\Users\\F\\Desktop\\papers\\sju-paper-scraper-app\\src\\modules\\top20.csv",
-    outputLocationPath="C:\\Users\\F\\Desktop\\papers\\sju-paper-scraper-app\\src\\modules\\",
+    inputFilePath="C:\\Users\\siotman\\Desktop\\Projects\\sju-paper-scraper-app\\src\\modules\\top20.csv",
+    outputLocationPath="C:\\Users\\siotman\\Desktop\\Projects\\sju-paper-scraper-app\\src\\modules\\",
+    # inputFilePath="C:\\Users\\F\\Desktop\\papers\\sju-paper-scraper-app\\src\\modules\\top20.csv",
+    # outputLocationPath="C:\\Users\\F\\Desktop\\papers\\sju-paper-scraper-app\\src\\modules\\",
     defaultQueryPackSize=200)
-    # filePath="C:\\Users\\siotman\\Desktop\\Projects\\sju-paper-scraper-app\\src\\modules\\top20.csv")
+    # filePath="")
 
     
