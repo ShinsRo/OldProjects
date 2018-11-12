4049e2e5af34f5947d32d65e1d4ba996e0e6f040
siotman <siotman0728@gmail.com>
2018-11-12 15:45:44 +0900

    build includes

--- src/dependencies.py
+++ src/dependencies.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 import xlwt
 import re
-
+# bs4, robobrowser, multiprocessing, xlwt, pandas, requests
 from bs4 import BeautifulSoup
 from robobrowser import RoboBrowser
 from multiprocessing import Process, Queue, Manager
