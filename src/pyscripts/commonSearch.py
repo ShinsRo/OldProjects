caa9820ac5fafe088b79fd37e56c1ad7e87f1d14
siotman0728@gmail.com <siotman0728@gmail.com>
2018-11-26 17:14:04 +0900

    일반 엑셀 검색 데모 완성

--- src/pyscripts/commonSearch.py
+++ src/pyscripts/commonSearch.py
@@ -5,10 +5,12 @@ import requests
 import pandas as pd
 import xlwt
 import re
+import random
 
 from bs4 import BeautifulSoup
 from robobrowser import RoboBrowser
 
+import concurrent.futures
 import sju_response
 
 class MultiSearch():
@@ -28,7 +30,7 @@ class MultiSearch():
         param += '&SID=' + self.SID
         self.browser.open(self.baseUrl + '/WOS_AdvancedSearch_input.do' + param)
 
-    def backToGeneralSearch(self):
+    def backToAdvancedSearch(self):
         sres = self.sres
 
         if self.searchCnt % 200 == 0:
@@ -79,12 +81,141 @@ class MultiSearch():
 
         return (queryList, words)
     
+    def getExcelFromWoS(self, url, mark, totalMarked, outputLocationPath):
+        mark_from = str(mark)
+        sres = self.sres
+
+        if mark + 499 > int(totalMarked):
+            mark_to = totalMarked
+        else:
+            mark_to = str(mark + 499)
+
+        sres.print(command='log', msg='[%s-%s레코드] 엑셀을 받을 준비를 합니다.'%(mark_from, mark_to))
+        
+        excelBrowser = RoboBrowser(history=True, parser='lxml')
+        excelBrowser.open(url)
+        reportLink = excelBrowser.select('a.citation-report-summary-link')
+
+        if len(reportLink) == 0: 
+            sres.print(command='err', msg='[%s-%s레코드] 요약 보고서가 없습니다.'%(mark_from, mark_to))
+            return None
+
+        sres.print(command='log', msg='[%s-%s레코드] 요약 보고서를 엽니다.'%(mark_from, mark_to))
+        excelBrowser.follow_link(reportLink[0])
+
+        summary_records_form = excelBrowser.get_form(id='summary_records_form')
+        qid = summary_records_form['qid'].value
+        filters = summary_records_form['filters'].value
+        sortBy = summary_records_form['sortBy'].value
+        timeSpan = summary_records_form['timeSpan'].value
+        endYear = summary_records_form['endYear'].value
+        startYear = summary_records_form['startYear'].value
+        rurl = summary_records_form['rurl'].value
+
+
+        piChart = summary_records_form['piChart'].value
+        toChart = summary_records_form['piChart'].value
+
+        makeExcelURL = "http://apps.webofknowledge.com/OutboundService.do?"
+        makeExcelParam = ""
+        makeExcelParam += "action=go"
+        makeExcelParam += "&save_options=xls"
+
+        makeExcelURL += makeExcelParam
+
+        sres.print(command='log', msg='[%s-%s레코드] 엑셀 데이터 제작을 요청합니다.'%(mark_from, mark_to))
+        excelBrowser.session.post(makeExcelURL, data={
+            "selectedIds": "",
+            "displayCitedRefs":"",
+            "displayTimesCited":"",
+            "displayUsageInfo":"true",
+            "viewType":"summary",
+            "product":"WOS",
+            "rurl":rurl,
+            "mark_id":"WOS",
+            "colName":"WOS",
+            "search_mode":"CitationReport",
+            "view_name":"WOS-CitationReport-summary",
+            "sortBy": sortBy,
+            "mode":"OpenOutputService",
+            "qid":qid,
+            "SID":self.SID,
+            "format":"crsaveToFile",
+            "mark_to":mark_to,
+            "mark_from":mark_from,
+            "queryNatural":"",
+            "count_new_items_marked":"0",
+            "use_two_ets":"false",
+            "IncitesEntitled":"no",
+            "value(record_select_type)":"range",
+            "markFrom":mark_from,
+            "markTo":mark_to,
+            "action":"recalulate",
+            "start_year_val":"1900",
+            "end_year_val":"2019",
+            "viewAbstractUrl":"",
+            "LinksAreAllowedRightClick": "full_record.do",
+            "filters":filters,
+            "timeSpan": timeSpan,
+            "db_editions": "",
+            "additional_qoutput_params": "cr_qid="+qid,
+            "print_opt":"Html",
+            "include_mark_from_in_url":"true",
+            "endYear":endYear,
+            "startYear":startYear,
+            "piChart":piChart,
+            "toChart":toChart,
+            "fields":"DUMMY_VALUE"
+        })
+
+        ExcelActionURL = "https://ets.webofknowledge.com"
+        ExcelAction = "/ETS/ets.do?"
+        
+        ExcelParam = "mark_from=1"
+        ExcelParam += "&product=UA"
+        ExcelParam += "&colName=WOS"
+        ExcelParam += "&displayUsageInfo=true"
+        ExcelParam += "&parentQid=" + qid
+        ExcelParam += "&rurl=" + requests.utils.quote(rurl)
+        ExcelParam += "&startYear=" + startYear
+        ExcelParam += "&mark_to=" + mark_to
+        ExcelParam += "&filters=" + requests.utils.quote(filters)
+        ExcelParam += "&qid=" + str(int(qid)+1)
+        ExcelParam += "&endYear=" + endYear
+        ExcelParam += "&SID=" + self.SID
+        ExcelParam += "&totalMarked=" + totalMarked
+        ExcelParam += "&action=crsaveToFile"
+        ExcelParam += "&timeSpan=" + requests.utils.quote(timeSpan)
+        ExcelParam += "&sortBy=" + sortBy
+        ExcelParam += "&displayTimesCited=false"
+        ExcelParam += "&displayCitedRefs=true"
+        ExcelParam += "&fileOpt=xls"
+        ExcelParam += "&UserIDForSaveToRID=null"
+
+        ExcelActionURL += ExcelAction
+        ExcelActionURL += ExcelParam
+
+        sres.print(command='log', msg='[%s-%s레코드] 엑셀 데이터를 다운로드 받습니다.'%(mark_from, mark_to))
+        res = requests.get(ExcelActionURL)
+        if res.text.find("<html>") > 0 or res.text.find("Error report</title>") > 0:
+            sres.print(command='err', msg='%s-%s 레코드, 서버가 에러를 반환'%(mark_from, mark_to))
+        
+        ofileName = "%X"%random.getrandbits(128)
+        with open(ofileName, 'wb') as rsFile:
+            rsFile.write(res.content)
+            rsFile.close()
+        resPD = pd.read_excel(ofileName, header=26)
+        os.remove(ofileName)
+
+        return resPD
+
     def generalSearch(
         self,
         startYear,
         endYear,
         gubun,
-        inputFilePath):
+        inputFilePath,
+        defaultQueryPackSize):
 
         sres = self.sres
         browser = self.browser
@@ -93,11 +224,10 @@ class MultiSearch():
         jsessionid = self.jsessionid
 
         sres.print(command='res', target='loading', res=True)
-
         sres.print('log', msg='단일 조회를 시작합니다.')
-        
+        self.searchCnt += 1
+
         WOS_AdvancedSearch_input_form = browser.get_form(id="WOS_AdvancedSearch_input_form")
-        defaultQueryPackSize = 0
         queryList, words = self.makeQueryFromFile(inputFilePath, defaultQueryPackSize, gubun)
 
         queryListLen = len(queryList)
@@ -115,8 +245,96 @@ class MultiSearch():
         historyResults = browser.select('div.historyResults')
         historyResultsLen = len(historyResults)
 
+        returnDict = {}
+        for idx, his in enumerate(historyResults):
+            totalMarked = his.a.text.replace(',', '')
+            url = self.baseUrl + his.a["href"]
+
+            if int(totalMarked) >= 10000:
+                sres.print(command='log', msg='해당 패키지 결과가 10000개 이상입니다.')
+                sres.print(command='log', msg='검색 범위를 좁혀주세요.')
+                self.backToAdvancedSearch()
+                return
+            outputLocationPath = os.path.dirname(inputFilePath)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=20) as excecuter:
+                future_report = {
+                    excecuter.submit(
+                        self.getExcelFromWoS, 
+                        url, mark, totalMarked, outputLocationPath): mark for mark in range(1, int(totalMarked), 500) 
+                }
+
+                for future in concurrent.futures.as_completed(future_report):
+                    mark_to = future_report[future]
+                    try:
+                        temp = future.result()
+                        if temp is not None: returnDict[mark_to] = temp
+                        sres.print(command='log', msg='%s번 레코드부터 성공적으로 데이터를 가져왔습니다.'%mark_to)
+                    
+                    except Exception as e:
+                        print(e)
+    
+        resPD = pd.concat(returnDict.values())
+
+        header = ""
+        if gubun == "TI": header = "Title"
+        elif gubun == "AU": header = "Authors"
+        elif gubun == "DO": header = "DOI"
+        else: pass
+        
+        if gubun is not 'AD':
+            notFoundList = []
+            resString = " ".join(resPD[header])
+            for word in words:
+                if not bool(re.search(word, resString, re.IGNORECASE)):
+                    notFoundList.append(word)
+
+            # notFoundDF = pd.DataFrame({
+            #     header: notFoundList
+            # })
+
+        # temp = "_all_succeed.xls"
+        # successResultFile = outputLocationPath + "/" + temp
+        # while(os.path.exists(successResultFile)):
+        #     temp = "_" + temp
+        #     successResultFile = outputLocationPath + "/" + temp
+
+        # excelWriter = pd.ExcelWriter(successResultFile)
+        # resPD.to_excel(excelWriter, '전체 검색 결과')
+        # excelWriter.save()
+
+        # temp = "_failed.xls"
+        # failedResultFile = outputLocationPath + "/" + temp
+        # while(os.path.exists(failedResultFile)):
+        #     temp = "_" + temp
+        #     failedResultFile = outputLocationPath + "/" + temp
+
+        # excelWriter = pd.ExcelWriter(failedResultFile)
+        # notFoundDF.to_excel(excelWriter, '전체 검색 실패 결과')
+        # excelWriter.save()
+        cResList = []
+        for idx in range(len(resPD['Title'])):
+            cResList.append({})
+            row = resPD.loc[idx]
+            cResList[idx]['id'] = '%d'%random.getrandbits(16)
+            cResList[idx]['DOI'] = row['DOI']
+            cResList[idx]['Title'] = row['Title']
+            cResList[idx]['Authors'] = row['Authors'].split(';')[:-1]
+            cResList[idx]['Source Title'] = row['Source Title']
+            cResList[idx]['Total Citations'] = str(row['Total Citations'])
+            cResList[idx]['Publication Date'] = row['Publication Date']
+            cResList[idx]['ivp'] = [
+                '%s/%s'%(row['Issue'], row['Volume']),
+                '%s-%s'%(row['Beginning Page'], row['Ending Page']),
+            ]
+        sres.print(
+            command='cres', 
+            target='result', 
+            res={
+                'cResList': cResList, 
+                'notFoundList': notFoundList
+            })
         sres.print(command='log', msg='논문들의 일반 정보를 가져왔습니다.')
-        self.backToGeneralSearch()
+        self.backToAdvancedSearch()
 
 if __name__ == "__main__":
     cres = sju_response.SJUresponse('test')
@@ -128,5 +346,6 @@ if __name__ == "__main__":
         startYear='2010',
         endYear='2018',
         gubun='TI',
-        inputFilePath='C:\\input.csv'
+        inputFilePath='C:\\input.csv',
+        defaultQueryPackSize=0
     )
--- src/renderer/components/commonSearchMultiView.vue
+++ src/renderer/components/commonSearchMultiView.vue
@@ -1,13 +1,25 @@
 <template>
   <v-layout row wrap>
     <!-- 옵션 컨테이너 -->
+    <v-flex xs4>
+      <v-select
+          :items="gubuns"
+          v-model="gubun"
+          label="검색구분"
+          outline
+        ></v-select>
+    </v-flex>
     <v-flex xs6>
-      <!-- <v-text-field
-        label="검색하실 내용을 입력하세요."
-        placeholder=""
-        outline
-        v-model=""
-      ></v-text-field> -->
+      <div v-if="file" class="text-xs-right" style="padding: 15px;">
+        {{file.path.slice(0,40)}} {{(file.path.length > 39)? "...":""}}
+      </div>
+      <div v-else class="text-xs-right" style="padding: 15px;">
+        파일 경로
+      </div>
+      
+    </v-flex>
+    <v-flex xs2 class="text-xs-right">
+      <file-select v-model="file"></file-select>
     </v-flex>
     <v-flex xs2>
       <v-text-field
@@ -25,7 +37,8 @@
         v-model="endYear"
       ></v-text-field>
     </v-flex>
-    <v-flex xs2 class="text-xs-center">
+    <v-spacer></v-spacer>
+    <v-flex xs2 class="text-xs-right">
       <v-btn
         @click="stdin"
         slot="activator"
@@ -33,9 +46,7 @@
       >
         불러오기
       </v-btn>
-      <v-flex v-if="loading">
-        <pulse-loader :loading="loading" :color="'#5bc0de'" :size="'20px'"></pulse-loader>
-      </v-flex>
+      <pulse-loader :loading="loading" :color="'#5bc0de'" :size="'20px'"></pulse-loader>
     </v-flex>
     <!-- END 옵션 컨테이너 -->
     <!-- 결과표 -->
@@ -77,6 +88,14 @@
           <v-alert slot="no-results" :value="true" color="error" icon="warning">
             Your search for "{{ listSearch }}" found no results.
           </v-alert>
+          <template slot="expand" slot-scope="props">
+            <h2 style="margin: 20px"><v-icon v-html="'assignment'"></v-icon> : {{ props.item['Title'] }}</h2>
+            <table style="border:1px solid #FFFFFF; margin: 20px; width: 100%">
+              <tr><td style="border:1px solid #FFFFFF;">이 논문의 저자 목록</td></tr>
+              <tr><td style="border:1px solid #FFFFFF;" :key="10">
+                {{ props.item['Authors'].join([separator = '; ']) }}</td></tr>
+            </table>
+          </template>
         </v-data-table>
       </v-card>
     </v-flex>
@@ -86,14 +105,23 @@
 
 <script>
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+import FileSelect from './commonSearchMultiView/FileSelect.vue';
 
 export default {
-  props: ['cResList', 'loading', 'executer', 'log'],
+  props: ['cResList', 'notFoundList', 'loading', 'executer', 'log'],
   data() {
     return {
       startYear: '2010',
       endYear: '2018',
-      gubun: 'TI',
+      gubun: '논문제목',
+      gubuns: ['논문제목', '저자명', '연구기관', 'DOI'],
+      gubunsId: {
+        논문제목: 'TI',
+        저자명: 'AU',
+        연구기관: 'AD',
+        DOI: 'DO',
+      },
+      file: null,
       inputFilePath: 'C:\\input.csv',
       listSearch: '',
       resHeaders: [
@@ -114,6 +142,7 @@ export default {
     };
   },
   components: {
+    FileSelect,
     PulseLoader,
   },
   methods: {
@@ -121,10 +150,46 @@ export default {
       this.log = '';
     },
     stdin() {
-      console.log('stdin func');
+      // 인풋값 체크
+      const errObj = {
+        code: 200,
+        msg: '입력 값이 잘못되었습니다',
+      };
+      try {
+        this.startYear.trim();
+        const intSY = parseInt(this.startYear, 10);
+        const intEY = parseInt(this.endYear, 10);
+        const intNY = parseInt((new Date()).getFullYear(), 10);
+        if (!this.startYear || this.startYear.length !== 4) {
+          errObj.code = 100;
+          errObj.msg = '시작년도를 확인해주세요.';
+        } else if (!this.endYear || this.endYear.length !== 4) {
+          errObj.code = 101;
+          errObj.msg = '끝 년도를 확인해주세요.';
+        } else if (
+          !(intSY >= 1900 && intSY <= intNY) ||
+          !(intEY >= 1900 && intEY <= intNY)
+        ) {
+          errObj.code = 102;
+          errObj.msg = '년도는 1900년에서 금년 + 1 사이여야 합니다.';
+        } else if (intSY > intEY) {
+          errObj.code = 103;
+          errObj.msg = '시작년도가 끝 년도보다 최근일 수 없습니다.';
+        } else if (this.file === null) {
+          errObj.code = 104;
+          errObj.msg = '파일 경로가 선택되지 않았습니다.';
+        } else {
+          this.inputFilePath = this.file.path;
+          errObj.code = false;
+        }
+        if (errObj.code) { throw errObj; }
+      } catch (e) {
+        alert(errObj.msg);
+        return;
+      }
       const payload = {
         scope: this,
-        inputs: ['multiCommonSearch', this.startYear, this.endYear, this.gubun, this.inputFilePath],
+        inputs: ['multiCommonSearch', this.startYear, this.endYear, this.gubunsId[this.gubun], this.inputFilePath, 0],
       };
       this.$emit('stdin', payload);
     },
