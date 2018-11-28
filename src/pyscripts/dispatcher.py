a7eb291dd83ae3ac3aa3299fb77401ac1b7c8f62
siotman0728@gmail.com <siotman0728@gmail.com>
2018-11-28 14:49:50 +0900

    인풋 유효성 체크

--- src/pyscripts/dispatcher.py
+++ src/pyscripts/dispatcher.py
@@ -2,15 +2,82 @@ import citationSearch
 import commonSearch
 import sju_response
 import concurrent.futures
+import datetime
+import os
+
+def inputValidation(dsres, serviceName):
+    dsres.print(command='log', msg='인풋 값을 검증합니다.')
+    now = datetime.datetime.now()
+    returnDict = {}
+    
+    # 단일 상세 검색
+    if serviceName == 'singleCitationSearch':
+        query = input().strip()
+        startYear = input().strip()
+        endYear = input().strip()
+
+        if not len(query) > 2: raise Exception('쿼리의 길이가 너무 짧습니다.')
+        if not 1900 <= int(startYear) <= now.year: raise Exception('시작년도가 올바르지 않습니다.')
+        if not 1900 <= int(endYear) <= now.year: raise Exception('끝 년도가 올바르지 않습니다.')
+        if not int(startYear) <= int(endYear): raise Exception('검색 기간이 올바르지 않습니다.')
+        returnDict['query'] = query 
+        returnDict['startYear'] = startYear 
+        returnDict['endYear'] = endYear 
+
+    # 다중 상세 검색
+    elif serviceName == 'multiCitationSearch':
+        startYear = input().strip()
+        endYear = input().strip()
+        gubun = input().strip()
+        path = input().strip()
+    
+        if not 1900 <= int(startYear) <= now.year: raise Exception('시작년도가 올바르지 않습니다.')
+        if not 1900 <= int(endYear) <= now.year: raise Exception('끝 년도가 올바르지 않습니다.')
+        if not int(startYear) <= int(endYear): raise Exception('검색 기간이 올바르지 않습니다.')
+        if not (gubun == 'TI' or gubun == 'DO'): raise Exception('구분이 올바르지 않습니다.')
+        if not (
+            path.endswith('.csv') 
+            or path.endswith('.xls')
+            or path.endswith('.xlsx')): raise Exception('파일 확장자가 올바르지 않습니다.')
+        returnDict['startYear'] = startYear 
+        returnDict['endYear'] = endYear 
+        returnDict['gubun'] = gubun 
+        returnDict['path'] = path 
+
+    # 다중 일반 검색
+    elif serviceName == 'multiCommonSearch':
+        startYear = input().strip()
+        endYear = input().strip()
+        gubun = input().strip()
+        inputFilePath = input().strip()
+        defaultQueryPackSize = input().strip()
+
+        if not 1900 <= int(startYear) <= now.year: raise Exception('시작년도가 올바르지 않습니다.')
+        if not 1900 <= int(endYear) <= now.year: raise Exception('끝 년도가 올바르지 않습니다.')
+        if not int(startYear) <= int(endYear): raise Exception('검색 기간이 올바르지 않습니다.')
+        if not (gubun == 'TI' or gubun == 'DO'): raise Exception('구분이 올바르지 않습니다.')
+        path = inputFilePath
+        if not (
+            path.endswith('.csv') 
+            or path.endswith('.xls')
+            or path.endswith('.xlsx')): raise Exception('파일 확장자가 올바르지 않습니다.')
+        returnDict['startYear'] = startYear 
+        returnDict['endYear'] = endYear 
+        returnDict['gubun'] = gubun 
+        returnDict['inputFilePath'] = path 
+        returnDict['defaultQueryPackSize'] = defaultQueryPackSize 
+    else:
+        raise Exception('인풋 값 검증 중 알 수 없는 오류')
+    return returnDict
 
 if __name__ == "__main__":
     dsres = sju_response.SJUresponse('dispatcher')
     dsres.print(command='res', target='loading', res=True)
     dsres.print(command='log', msg='dispatcher를 준비합니다.')
-
     singleCitationSearchObj = None
     multiCitationSearchObj = None
     multiCommonSearchObj = None
+
     # 서비스 초기화
     try:
         serviceList = [
@@ -62,12 +129,20 @@ if __name__ == "__main__":
         # 검색 서비스 종류
         serviceName = input().strip()
         dsres.print(command='res', target='loading', res=True)
+        
+        inputs = ''
+        try:
+            inputs = inputValidation(dsres, serviceName)
+        except Exception as e:
+            dsres.print(command='err', msg='인풋 값이 유효하지 않습니다.')
+            dsres.print(command='err', msg=str(e))
+            continue
 
         # 단일 상세 검색
         if serviceName == 'singleCitationSearch':
-            query = input().strip()
-            startYear = input().strip()
-            endYear = input().strip()
+            query = inputs['query']
+            startYear = inputs['startYear']
+            endYear = inputs['endYear']
             
             try:
                 singleCitationSearchObj.generalSearch(
@@ -86,10 +161,10 @@ if __name__ == "__main__":
 
         # 다중 상세 검색
         elif serviceName == 'multiCitationSearch':
-            startYear = input().strip()
-            endYear = input().strip()
-            gubun = input().strip()
-            path = input().strip()
+            startYear = inputs['startYear']
+            endYear = inputs['endYear']
+            gubun = inputs['gubun']
+            path = inputs['path']
 
             try:
                 multiCitationSearchObj.generalSearch(
@@ -103,15 +178,13 @@ if __name__ == "__main__":
             else:
                 dsres.print(command='log', msg='상세 엑셀 검색이 완료되었습니다.')
 
-            
-        
         # 다중 일반 검색
         elif serviceName == 'multiCommonSearch':
-            startYear = input().strip()
-            endYear = input().strip()
-            gubun = input().strip()
-            inputFilePath = input().strip()
-            defaultQueryPackSize = input().strip()
+            startYear = inputs['startYear']
+            endYear = inputs['endYear']
+            gubun = inputs['gubun']
+            inputFilePath = inputs['inputFilePath']
+            defaultQueryPackSize = inputs['defaultQueryPackSize']
 
             try:
                 multiCommonSearchObj.generalSearch(
