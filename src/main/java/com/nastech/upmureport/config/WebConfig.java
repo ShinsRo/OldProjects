93bb8049b4dd11d14c98c9e4cc71861737adc5a3
sskim <sskim@nastech.co.kr>
2019-03-07 17:47:02 +0900

    WebMvcConfigurerAdapter(deprecated) -> WebMvcConfigurer

--- src/main/java/com/nastech/upmureport/config/WebConfig.java
+++ src/main/java/com/nastech/upmureport/config/WebConfig.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.nastech.upmureport", "com.nastech.upmureport.jpa"})
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void configureViewResolvers (ViewResolverRegistry registry) {
