4e6629e067b768810c1fe8614cbb7d085c0ce5aa
sskim <sskim@nastech.co.kr>
2019-03-07 16:18:10 +0900

    a

--- src/main/java/com/nastech/upmureport/config/WebConfig.java
+++ src/main/java/com/nastech/upmureport/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.nastech.upmureport.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,5 +15,15 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void configureViewResolvers (ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/jsp/", ".jsp");
+	}
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	    registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+	    registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
+	    registry.addResourceHandler("/img/**").addResourceLocations("/resources/img/");
+	    registry.addResourceHandler("/scss/**").addResourceLocations("/resources/scss/");
+	    registry.addResourceHandler("/vendor/**").addResourceLocations("/resources/vendor/");
 	}
 }
