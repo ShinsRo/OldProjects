a901ddf50206595234fa5283886603edb65d74fe
sskim <sskim@nastech.co.kr>
2019-03-07 09:47:34 +0900

    데이터베이스 DDL 정리 문서 초기화

---
+++ src/main/resources/SQL/UPMUREPORT_DDL.sql
@@ -0,0 +1,25 @@
+/*
+ * subject 	: 
+ * date 	: 2019-03-07
+ * author 	: 마규석
+ * desc.	: 
+ */
+
+
+
+
+/*
+ * subject 	: 
+ * date 	: 2019-03-07
+ * author 	: 김승신
+ * desc.	: 
+ */
+
+
+
+/*
+ * subject 	: 
+ * date 	: 2019-03-07
+ * author 	: 김윤상
+ * desc.	: 
+ */
