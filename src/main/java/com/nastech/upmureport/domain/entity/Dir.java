052356d87555ee0cb673acec150b52de96aabe9c
sskim <sskim@nastech.co.kr>
2019-03-08 16:09:11 +0900

    child dir list

--- src/main/java/com/nastech/upmureport/domain/entity/Dir.java
+++ src/main/java/com/nastech/upmureport/domain/entity/Dir.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -35,6 +36,6 @@ public class Dir {
 	@JoinColumn(name = "dir_id")
 	private Dir parentDir;
 	
-//	@OneToMany(mappedBy = "dir")
-//	private List<Dir> childDirs = new ArrayList<Dir>(); 
+	@OneToMany(mappedBy = "dir")
+	private List<Dir> childDirs = new ArrayList<Dir>(); 
 }
