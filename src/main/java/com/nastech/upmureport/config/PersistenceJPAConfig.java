132b06c33f2de0a4053d21d6fc298c8f9212f4db
yskim <yskim@nastech.co.kr>
2019-03-13 10:19:53 +0900

    c

--- src/main/java/com/nastech/upmureport/config/PersistenceJPAConfig.java
+++ src/main/java/com/nastech/upmureport/config/PersistenceJPAConfig.java
@@ -61,7 +61,7 @@ public class PersistenceJPAConfig {
 	
 	Properties additionalProperties() {
 		Properties properties = new Properties();
-		properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		return properties;
 	}
