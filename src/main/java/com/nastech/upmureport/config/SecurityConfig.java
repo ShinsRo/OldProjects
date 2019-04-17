e684a2a9e883cf189cf39f6fc57671c2fda5913a
ksma <ksma@nastech.co.kr>
2019-04-17 10:57:27 +0900

    0417_REST_Ing

--- src/main/java/com/nastech/upmureport/config/SecurityConfig.java
+++ src/main/java/com/nastech/upmureport/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.nastech.upmureport.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,13 +24,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      
 	@Override
      protected void configure(HttpSecurity http) throws Exception {
-    	 http
-         .csrf().disable()
-         .authorizeRequests()
-              .anyRequest().authenticated()
-              .and()
-         .formLogin()
-         .usernameParameter("username").passwordParameter("password").defaultSuccessUrl("/test").permitAll();             
+		http
+		.csrf().disable()
+		.authorizeRequests()
+			.antMatchers("/api/users/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.usernameParameter("username").passwordParameter("password").defaultSuccessUrl("/test").permitAll();             
      }
 
 
@@ -43,6 +45,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      @Bean
      public PasswordEncoder passwordEncoder() {
     	 return new BCryptPasswordEncoder();
+     }
+     
+     @Bean
+     @Override
+     public AuthenticationManager authenticationManagerBean() throws Exception {
+          return super.authenticationManagerBean();
      }
      
 }
