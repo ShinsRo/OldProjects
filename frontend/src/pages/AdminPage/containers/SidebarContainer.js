051f686bdcbd408a2f7bdec29129e2680b33e894
ksma <ksma@nastech.co.kr>
2019-05-09 09:55:55 +0900

    adminPage 변경/ Profile 변경/ Register 모달 추가_ID 중복체크 추가/Dept,Posi 모달추가/ 등등

--- frontend/src/pages/AdminPage/containers/SidebarContainer.js
+++ frontend/src/pages/AdminPage/containers/SidebarContainer.js
@@ -63,13 +63,18 @@ class SidebarContainer extends Component {
         const {userState} = store.getState()
         const {userInfo} = userState
         const {memberInfo} = userInfo
+        const {posiList} = this.props;
+        const {deptList} = this.props;
         //const deptName=this.state.deptName
+        //console.log("사이드바 컨테이너",this.props)
         return(
             <Sidebar
                 users={users}
                 depts={depts}
                 select={select}
                 userInfo={memberInfo}
+                deptList={deptList}
+                posiList={posiList}
                 //deptName={deptName}
             />
         );
