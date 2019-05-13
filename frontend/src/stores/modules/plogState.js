16bfce8811991e70b69852c615f1cd72dc793917
김승신 <sskim@nastech.co.kr>
2019-05-13 09:58:23 +0900

    Revert "Merge branch 'front-PLog' into 'dev'"
    This reverts merge request !55

--- frontend/src/stores/modules/plogState.js
+++ frontend/src/stores/modules/plogState.js
@@ -5,29 +5,21 @@ import * as plogService from '../../api/PLogService';
 const PLOG_SEND_PENDING = 'PLOG_SEND_PENDING';
 const PLOG_SEND_SUCCESS = 'PLOG_SEND_SUCCESS';
 const PLOG_SEND_ERROR = 'PLOG_SEND_ERROR';
-// const SET_PFILE_LOG = 'SET_PFILE_LOG';
-// const SET_ATTACHMENT_LOG = 'SET_ATTACHMENT_LOG';
-
-
-const SET_LOG = 'SET_LOG';
-const SET_LOG_VIEW_LEVEL = 'SET_LOG_VIEW_LEVEL';
+const SET_PFILE_LOG = 'SET_PFILE_LOG';
+const SET_ATTACHMENT_LOG = 'SET_ATTACHMENT_LOG';
 
 export const plogPending = createAction(PLOG_SEND_PENDING);
 export const plogSendSuccess = createAction(PLOG_SEND_SUCCESS);
 export const plogSenderr = createAction(PLOG_SEND_ERROR);
-// export const setPfileLog = createAction(SET_PFILE_LOG);
-// export const setAttachmentLog = createAction(SET_ATTACHMENT_LOG);
-
-export const setLog = createAction(SET_LOG);
-export const setLogViewLevel = createAction(SET_LOG_VIEW_LEVEL);
+export const setPfileLog = createAction(SET_PFILE_LOG);
+export const setAttachmentLog = createAction(SET_ATTACHMENT_LOG);
 
 
 const initialState = Map({
     isFetching: false,
     error: false,
-    // pfileLogs: [],
-    // attachmentLogs: [],
-    logViewLevel : 'logList',
+    pfileLogs: [],
+    attachmentLogs: [],
 })
 
 export const getPLog = (pid) => dispatch => {
@@ -52,17 +44,11 @@ export default handleActions({
     [PLOG_SEND_ERROR]: (state, action) => {
         return state.set('isFetching', false).set('error', true);
     },
-    // [SET_PFILE_LOG]: (state, action) => {
-    //     return state.set('pfileLog', action.payload);
-    // },
-    // [SET_ATTACHMENT_LOG]: (state, action) => {
-    //     return state.set('attachmentLog', action.payload);
-    // },
-    [SET_LOG]: (state, action) => {
-            return state.set('pLog', action.payload);
-        },
-    [SET_LOG_VIEW_LEVEL]: (state, action) => {
-        return state.set('logViewLevel', action.payload);
+    [SET_PFILE_LOG]: (state, action) => {
+        return state.set('pfileLog', action.payload);
+    },
+    [SET_ATTACHMENT_LOG]: (state, action) => {
+        return state.set('attachmentLog', action.payload);
     },
 
 }, initialState);
