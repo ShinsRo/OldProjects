66d9bdfb9c986f30feebe6c0e90e616f8c715e1a
yskim <yskim@nastech.co.kr>
2019-05-13 11:32:55 +0900

    수동 머지

--- src/test/java/com/nastech/upmureport/LogTest.java
+++ src/test/java/com/nastech/upmureport/LogTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.nastech.upmureport.service.PfileService;
 
@@ -22,6 +23,6 @@ public class LogTest {
 	@Test
 	public void logTest() {
 		LOG.info("info log");
-		pfileService.apoTest();
+		//pfileService.apoTest();
 	}	
 }
