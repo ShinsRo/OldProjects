94c8cb0cc8f27f155d011afe223355dd5502e0af
ksma <ksma@nastech.co.kr>
2019-05-13 09:44:50 +0900

    Career Cardbody modify

--- frontend/src/pages/AdminPage/components/Admin/Career.js
+++ frontend/src/pages/AdminPage/components/Admin/Career.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { BASE_URL } from '../../../../supports/API_CONSTANT';
-import {MDBBtn,MDBIcon} from 'mdbreact'
+import { MDBBtn, MDBIcon } from 'mdbreact'
 
 class Career extends Component {
   constructor(props) {
@@ -10,7 +10,7 @@ class Career extends Component {
     this.state = { value: '', posi: '' };
   }
 
-  onChange(e,target) {
+  onChange(e, target) {
     this.setState({
       [target]: e.target.value
     })
@@ -26,7 +26,7 @@ class Career extends Component {
       dept: this.state.value,
       posi: this.state.posi
     }
-    if( careerInfo.dept==="" | careerInfo.posi===""){
+    if (careerInfo.dept === "" | careerInfo.posi === "") {
       return alert("변경할 커리어를 선택하세요")
     }
 
@@ -37,13 +37,13 @@ class Career extends Component {
       }
     ).then(
       (response) => {
-        if(!response.data) alert("잘못 된 요청입니다")
-        else{
-          alert("부서:"+response.data.dept+"\n직책:"+response.data.posi+"\n변경 되었습니다")
-          window.location.href="/adminpage";
+        if (!response.data) alert("잘못 된 요청입니다")
+        else {
+          alert("부서:" + response.data.dept + "\n직책:" + response.data.posi + "\n변경 되었습니다")
+          window.location.href = "/adminpage";
         }
       }
-      
+
     )
   }
 
@@ -70,8 +70,7 @@ class Career extends Component {
 
     return (
 
-      <div className="card mb-4" align="">
-
+      <div className="card" align="">
         <h className="card-header font-weight-bold text-black">{selectUser1.name}님 커리어 </h>
 
         <div className="card-body">
@@ -79,20 +78,12 @@ class Career extends Component {
             <div className="col mr-2">
               <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">시작일</div>
               <div className="h5 mb-0 font-weight-bold text-gray-800">{currentCarrer && currentCarrer.startDate}</div>
-            </div>
 
-          </div>
-        </div>
-
-        <div className="card-body">
-          <div className="row no-gutters align-items-center">
-            <div className="col mr-2">
-              <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">부서</div>
+              <div className="text-xs font-weight-bold text-primary text-uppercase mt-3 mb-1">부서</div>
               <div className="h5 mb-0 font-weight-bold text-gray-800"> {currentCarrer && currentCarrer.dept}</div>
               <div className="h5 mb-0 font-weight-bold text-gray-800">
-
                 <form action="">
-                  <select value={this.state.value} onChange={e => this.onChange(e,'value')}>
+                  <select value={this.state.value} onChange={e => this.onChange(e, 'value')}>
                     {
                       deptList && deptList.map(dept => {
                         return (
@@ -100,22 +91,11 @@ class Career extends Component {
                         )
                       })
                     }
-                    {/* <option value="grapefruit">{currentCarrer&& currentCarrer.dept}</option>
-                                <option value="lime">Lime</option>
-                                <option value="coconut">Coconut</option>
-                                <option value="mango">Mango</option> */}
                   </select>
                 </form>
               </div>
-            </div>
 
-          </div>
-        </div>
-
-        <div className="card-body">
-          <div className="row no-gutters align-items-center">
-            <div className="col mr-2">
-              <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">직책</div>
+              <div className="text-xs font-weight-bold text-primary text-uppercase mt-3 mb-1">직책</div>
               <div className="h5 mb-0 font-weight-bold text-gray-800"> {currentCarrer && currentCarrer.posi}</div>
               <div className="h5 mb-0 font-weight-bold text-gray-800">
 
@@ -135,12 +115,12 @@ class Career extends Component {
                   </select>
                 </form>
               </div>
+              
             </div>
-
           </div>
         </div>
         {/* <input type="button" value=" Change " name="dept" onClick={this.changeCareerAPI.bind(this)} class="btn btn-primary btn-icon-split"></input> */}
-        <MDBBtn outline rounded size="sm" className="" color="primary" onClick={this.changeCareerAPI.bind(this)}><MDBIcon icon="check" className=""/>  변경  </MDBBtn>
+        <MDBBtn outline rounded size="sm" className="" color="primary" onClick={this.changeCareerAPI.bind(this)}><MDBIcon icon="check" className="" />  변경  </MDBBtn>
         {/* <div className="card-body">  직접 입력 버전
           <div className="row no-gutters align-items-center">
             <div className="col mr-2">
