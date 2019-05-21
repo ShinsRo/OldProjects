e2b2ed70fcd3aa708c60990185fb5d4ce57b5a55
siotman0728 <siotman0728@gmail.com>
2019-05-21 11:12:37 +0900

    프로젝트 메세징(소켓) 서비스 에러처리
    1. 유효하지 않은 DTO 인자
    2. 유효하지 않은 멤버 아이디
    3. 만료된 세션
    
    에 대한 에러처리 완료

--- src/main/java/com/nastech/upmureport/service/ProjectNotificationService.java
+++ src/main/java/com/nastech/upmureport/service/ProjectNotificationService.java
@@ -3,6 +3,8 @@ package com.nastech.upmureport.service;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import com.nastech.upmureport.config.MessageSession;
 import com.nastech.upmureport.config.MessageSessionContainer;
+import com.nastech.upmureport.controller.ProjectController;
 import com.nastech.upmureport.domain.dto.CollaboratorDto;
 import com.nastech.upmureport.domain.dto.ProjectDto;
 
@@ -20,7 +23,10 @@ public class ProjectNotificationService {
 	public enum NOTIFICATION_TYPE {
 		NEW_PROJECT,
 		CORRECT_PROJECT,
+		NOTIFY,
+		CHAT,
 	}
+	Log Logger = LogFactory.getLog(ProjectController.class);
 	
 	@Autowired
 	MessageSessionContainer msc;
@@ -45,24 +51,37 @@ public class ProjectNotificationService {
 	 * @param fromMid	보내는 이 멤버 식별 아이디
 	 * @param dto		보낼 DTO		
 	 * @param type		메세지 타입
+	 * @throws Exception 
 	 */
-	public <MSG> void sendDtoByType(String fromMid, MSG dto, NOTIFICATION_TYPE type) {
-		Map<String, MessageSession> sessionMap 	= msc.getContainer();				// 현재 접속 중인 세션 맵 { mid : MessageSession }
+	public <MSG> void sendDtoByType(String fromMid, MSG dto, NOTIFICATION_TYPE type) throws Exception {
+		// 현재 접속 중인 세션 맵 { mid : MessageSession }
+		Map<String, MessageSession> sessionMap 	= msc.getContainer();				
+
+		/* 유효하지 않은 DTO 타입 */
+		if (dto == null) {
+			Logger.error("인자 dto는 NULL일 수 없습니다.");
+			throw new IllegalDtoArgException();
+		}
+		
+		/* 유효하지 않은(이미 연결을 해제한) 멤버 식별 아이디 */
+		if (!sessionMap.containsKey(fromMid)) {
+			Logger.error("유효하지 않은 mid입니다.");
+			throw new SessionNotExistsExcepiton();
+		}
+		
+		/* 만료된 세션 */
+		if (sessionMap.get(fromMid).isExpired()) {
+			Logger.error("만료된 세션으로부터의 요청입니다.");
+			throw new SessionExpiredExcepiton();
+		}
 		
 		switch(type) {
 		case NEW_PROJECT: case CORRECT_PROJECT:
-			
-			/* 에러처리 시작 */
-			if (dto == null || !(dto instanceof ProjectDto)) {
-				/* 올바르지 않은 인자 */
-				break;
+			/* 유효하지 않은 DTO 타입 */
+			if (!(dto instanceof ProjectDto)) { 
+				Logger.error("메세지 타입과 다른 타입의 DTO입니다.");
+				throw new IllegalDtoArgException();
 			}
-			
-			if (!sessionMap.containsKey(fromMid)) {
-				/* 유효하지 않은(이미 연결을 해제한) 멤버 식별 아이디 */
-				break;
-			}
-			/* 에러처리 시작 끝 */
 			
 			ProjectDto projectDto 					= (ProjectDto) dto;					// 보낼 프로젝트 내용
 			List<CollaboratorDto> collaborators 	= projectDto.getCollaborators();	// 프로젝트에서 관련한 협업자 리스트
@@ -73,8 +92,8 @@ public class ProjectNotificationService {
 			for (CollaboratorDto collaboratorDto : collaborators) {
 				String collabMid = collaboratorDto.getMid();	// 메세지 받을 이 mid
 				
-				if (collaboratorDto.getMid().equals(fromMid)) 	continue;
-				if (!sessionMap.containsKey(collabMid)) 		continue;
+				if (collaboratorDto.getMid().equals(fromMid)) 	continue;					// 스스로에 대해 처리하지 않는다.
+				if (!sessionMap.containsKey(collabMid)) 		continue;					// 접속 중이지 않은 멤버는 처리하지 않는다.
 				
 				MessageSession collaboratorSession 	= sessionMap.get(collabMid);			// 받을 이
 				String collaboratorSessionId 		= collaboratorSession.getSessionId();	// 받을 이 소켓 세션 아이디
@@ -104,5 +123,16 @@ public class ProjectNotificationService {
 		ha.setHeader("TYPE", type);
 		ha.setLeaveMutable(true);
 		return ha.getMessageHeaders();
+	}
+	
+	/* 고유 익셉션 정의 */
+	public class IllegalDtoArgException extends Exception {
+		private static final long serialVersionUID = 1L;
+	}
+	public class SessionNotExistsExcepiton extends Exception {
+		private static final long serialVersionUID = 1L;
+	}
+	public class SessionExpiredExcepiton extends Exception {
+		private static final long serialVersionUID = 1L;
 	}
 }
