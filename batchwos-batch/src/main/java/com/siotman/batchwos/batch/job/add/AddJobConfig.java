a76156644d77fc374d481453c827f66589402a24
siotman_mac <siotman0728@gmail.com>
2019-07-22 11:54:47 +0900

    Closed #1 - StaxEventItemReader, Required a resource. 에러 해결
    Builder를 사용해서 이러한 현상이 나타난 것임
    MultiResourceItemReader의Deligate로 사용할 때는 빌더로 만들면 안됨

--- batchwos-batch/src/main/java/com/siotman/batchwos/batch/job/add/AddJobConfig.java
+++ batchwos-batch/src/main/java/com/siotman/batchwos/batch/job/add/AddJobConfig.java
@@ -1,50 +1,42 @@
 package com.siotman.batchwos.batch.job.add;
 
-import com.siotman.batchwos.batch.domain.xml.XmlListField;
-import com.siotman.batchwos.batch.domain.xml.XmlRecord;
-import com.siotman.batchwos.batch.domain.xml.XmlStringField;
+import com.siotman.batchwos.batch.domain.xml.Record;
 import com.siotman.batchwos.wsclient.WsUtil;
 import com.siotman.batchwos.wsclient.search.SearchClient;
 import com.siotman.batchwos.wsclient.search.domain.SearchResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.ItemReadListener;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.batch.item.file.builder.MultiResourceItemReaderBuilder;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
-import org.springframework.batch.jsr.item.ItemWriterAdapter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.oxm.xstream.XStreamMarshaller;
 
 import javax.xml.soap.SOAPMessage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.HashMap;
+import java.io.*;
 import java.util.List;
-import java.util.Map;
 
 @Configuration
-public class AddNewRecordsJobConfig {
-    private String fSep = System.getProperty("file.separator");
+public class AddJobConfig {
 
-    private Logger logger = LoggerFactory.getLogger(AddNewRecordsJobConfig.class);
+    private Logger logger = LoggerFactory.getLogger(AddJobConfig.class);
+
     private static final Long WS_SEARCH_CHUNK = Long.valueOf(100);
     private static final String WS_SEARCH_TIMESPAN_BEGIN = "1945-01-01";
     private static final String WS_SEARCH_TIMESPAN_END = "2019-01-01";
+
+    private String fSep = System.getProperty("file.separator");
 
     @Autowired
     private SearchClient searchClient;
@@ -83,6 +75,8 @@ public class AddNewRecordsJobConfig {
                 })).build();
     }
 
+
+    /* retrieveNewRecordsStep 시작 */
     @Bean
     @JobScope
     public Step retrieveNewRecordsStep() {
@@ -92,9 +86,11 @@ public class AddNewRecordsJobConfig {
                     Long firstRecord = (Long) chunkContext.getAttribute("firstRecord");
                     if (firstRecord == null) firstRecord = Long.valueOf(1);
 
+                    logger.info(String.format("%d번 레코드로부터 리트리브", firstRecord));
+
                     SOAPMessage response = searchClient.retrieve(
                             currentSearchResponse.getQueryId(),
-                            Long.valueOf(2), WS_SEARCH_CHUNK,
+                            firstRecord, WS_SEARCH_CHUNK,
                             null, null, null
                     );
 
@@ -109,119 +105,86 @@ public class AddNewRecordsJobConfig {
                     }
                     FileOutputStream fos = new FileOutputStream(responseFile);
 
-                    WsUtil.printSOAPMessage(response, "", new PrintStream(fos));
+                    WsUtil.printSOAPBody(response, new PrintStream(fos));
 
                     firstRecord += WS_SEARCH_CHUNK;
                     chunkContext.setAttribute("firstRecord", firstRecord);
 
-//                    Long recordsFound = Long.valueOf(currentSearchResponse.getRecordsFound());
-                    Long recordsFound = Long.valueOf(30);
+                    Long recordsFound = Long.valueOf(currentSearchResponse.getRecordsFound());
+//                    Long recordsFound = Long.valueOf(3);
                     if (firstRecord > recordsFound) return RepeatStatus.FINISHED;
                     else return RepeatStatus.CONTINUABLE;
                 })).build();
     }
+    /* retrieveNewRecordsStep 끝 */
 
+
+    /*
+    * convertAndInsertStep 시작
+    *
+    * searchResponseXmlReader
+    *   - recordMarshaller
+    * Xml2EntityProcessor
+    * JpaWriter
+    * */
     @Bean
     @JobScope
     public Step convertAndInsertStep() {
         return this.stepBuilderFactory.get("convertAndInsertStep")
                 .chunk(50)
-                .reader(multiSearchResponseItemReader())
-                .listener(lisntener())
+                .reader(searchResponseXmlReader())
                 .writer(writer())
                 .build();
     }
 
     @Bean
-    public ItemWriter writer() {
-        return new ItemWriterTemp();
-    }
+    public MultiResourceItemReader<Record> searchResponseXmlReader() {
+        Resource[] resources = tempResourcesResolver
+                .loadResources("file:./target/temp/fetched_*.xml");
 
-    public class ItemWriterTemp implements ItemWriter {
+        XStreamMarshaller marshaller = new XStreamMarshaller();
 
-        @Override
-        public void write(List list) throws Exception {
-            System.out.println("wiriririr");
-        }
-    }
+        marshaller.setAnnotatedClasses(Record.class);
+        marshaller.setAutodetectAnnotations(true);
 
-    @Bean
-    public TempLisntener lisntener() {
-        return new TempLisntener();
-    }
+        StaxEventItemReader<Record> xmlReader = new StaxEventItemReader<>();
+        xmlReader.setFragmentRootElementName("records");
+        xmlReader.setUnmarshaller(marshaller);
 
-    public class TempLisntener implements ItemReadListener<XmlRecord> {
+        MultiResourceItemReader<Record> reader = new MultiResourceItemReader<>();
+        reader.setDelegate(xmlReader);
+        reader.setName("searchResponseXmlReader");
+        reader.setResources(resources);
 
-        @Override
-        public void beforeRead() {
-
-        }
-
-        @Override
-        public void afterRead(XmlRecord xmlRecord) {
-            System.out.println(xmlRecord.getUid());
-            xmlRecord.getXmlStringFields().stream().forEach(field -> {
-                System.out.println(field.getLabel() + " : " + field.getValue());
-            });
-            xmlRecord.getXmlListFields().stream().forEach(field -> {
-                System.out.println(field.getLabel() + " : " + field.getValue().get(0));
-            });
-        }
-
-        @Override
-        public void onReadError(Exception e) {
-
-        }
-    }
-
-    @Bean
-    public StaxEventItemReader searchResponseItemReader() {
-        return new StaxEventItemReaderBuilder<XmlRecord>()
-                .name("searchResponseItemReader")
-                .addFragmentRootElements("records")
-                .resource(new FileSystemResource("target/temp/fetched_1.xml"))
-                .unmarshaller(recordMarshaller())
-                .build();
+        return reader;
     }
 
     @Bean
     public XStreamMarshaller recordMarshaller() {
-        Map<String, Class> aliases = new HashMap<>();
-        aliases.put("records", XmlRecord.class);
-        aliases.put("uid", String.class);
-        aliases.put("label", String.class);
-        aliases.put("value", String.class);
-        aliases.put("title", XmlStringField.class);
-        aliases.put("doctype", XmlStringField.class);
-        aliases.put("source", XmlStringField.class);
-        aliases.put("other", XmlStringField.class);
-        aliases.put("authors", XmlListField.class);
-        aliases.put("keywords", XmlListField.class);
-
         XStreamMarshaller marshaller = new XStreamMarshaller();
-        marshaller.setAliases(aliases);
+
+        marshaller.setAnnotatedClasses(Record.class);
+        marshaller.setAutodetectAnnotations(true);
+
         return marshaller;
     }
+    /* convertAndInsertStep 끝 */
+
 
     @Bean
-    public MultiResourceItemReader multiSearchResponseItemReader() {
-        return new MultiResourceItemReaderBuilder<>()
-                .delegate(searchResponseItemReader())
-                .name("multiSearchResponseItemReader")
-                .resources(tempResourcesResolver
-                        .loadResources("target/temp/fetched_*.xml"))
-                .build();
+    public ItemWriter writer() {
+        return new ItemWriterTemp();
+    }
+    public class ItemWriterTemp implements ItemWriter {
+
+        @Override
+        public void write(List list) throws Exception {
+            System.out.println("======writer======");
+            list.stream().forEach(record -> {
+                System.out.println(((Record) record).getUid());
+            });
+        }
     }
 
-    @Bean
-    @JobScope
-    public Step deleteResourcesStep() {
-        return this.stepBuilderFactory.get("deleteResourcesStep")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("testStepTasklet");
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-
-
+    //
 }
--- batchwos-batch/src/test/java/com/siotman/batchwos/batch/BatchwosBatchApplicationTests.java
+++ batchwos-batch/src/test/java/com/siotman/batchwos/batch/BatchwosBatchApplicationTests.java
@@ -2,15 +2,36 @@ package com.siotman.batchwos.batch;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BatchwosBatchApplicationTests {
 
+    @Autowired
+    ResourceLoader resourceLoader;
+
     @Test
     public void contextLoads() {
-    }
+        Resource resource = resourceLoader.getResource("file:./target/temp/fetched_1.xml");
+        System.out.println(resource.isReadable());
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(resource.getFile()));
 
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
