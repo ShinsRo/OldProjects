d22f8380043fa7288b094a36220a3f4b80c7b5a4
김승신 <siotman0728@gmail.com>
2019-09-22 21:05:46 +0900

    로컬 메세지 서버 -> 개인 서버 메세지 서버로 변경

--- python-parser/tests/messageProdTest.py
+++ python-parser/tests/messageProdTest.py
@@ -2,15 +2,16 @@
 
 import pika
 
-RABBITMQ_SERVER_URL = 'amqp://sejong:sejong1234@127.0.0.1:5672/'
+RABBITMQ_SERVER_URL = 'amqp://remote-dev:compact@www.siotman.com:15673/'
 
 if __name__ == "__main__":
     connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_SERVER_URL))
     channel = connection.channel()
     for idx in range(0,10):
         msg = "detail$,uid$,SSSIIIDDD$,http://testest%d.com$,상태"%idx
-        channel.basic_publish(exchange='create',
-                      routing_key='target.create.test',
+        channel.basic_publish(exchange='test-ex',
+                      routing_key='test.log',
                       body=msg)
         print(msg)
 
+ 
