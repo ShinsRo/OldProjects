56086205f6b455cb1ae489fe2606307bcb8ac2af
squeen <shelltlove1@naver.com>
2019-10-15 16:45:35 +0900

    Fix a style sheet of header layout (css to scss)
    헤더 레이아웃 스타일을 css에서 scss로 수정하였음

--- your-paper-fe/src/components/Main/header/HeaderLayout.vue
+++ your-paper-fe/src/components/Main/header/HeaderLayout.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <div id="headerLogoLayout">
-        <p>
+        <p class="logoName">
           Your Paper
         </p>
 
@@ -9,27 +9,27 @@
       <div id="headerSettingLayout">
         <div id="headerSettingTabLayout">
           <div class="headerSettingTab">
-            <p>
+            <p class="text">
               My Paper
             </p>
           </div>
           <div class="headerSettingTab">
-            <p>
+            <p class="text">
               Paper Statics
             </p>
           </div>
         </div> <!--헤더 설정 화면탭 레이아웃-->
         <div id="headerSettingUserLayout">
           <div class="headerSettingUser">
-            <img src="../../../assets/image/image-03.png"/>
+            <img class="userImage" src="../../../assets/image/image-03.png"/>
           </div>
           <div class="headerSettingUser">
-            <p>
+            <p class="text">
               SuHyoun
             </p>
           </div>
           <div class="headerSettingUser">
-            <p>
+            <p class="text">
               menu
             </p>
           </div>
@@ -46,64 +46,6 @@
     }
 </script>
 
-<style scoped>
-  #headerLogoLayout{
-    display: flex;
-    align-items : center;
-    justify-content: center;
-    width: 10%;
-    height: 100%;
-  }
-  #headerLogoLayout > p {
-    color: white;
-    font-size: 20px;
-  }
-
-  #headerSettingLayout{
-    display: flex;
-    float: right;
-    width: 40%;
-    height: 100%;
-    justify-self: flex-end;
-  }
-
-  #headerSettingTabLayout{
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 60%;
-  }
-
-  .headerSettingTab > p{
-    color: white;
-    font-size: 20px;
-  }
-
-  #headerSettingUserLayout{
-    display: flex;
-    width: 40%;
-    justify-content: space-around;
-    align-items: center;
-  }
-
-  .headerSettingUser {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 80%;
-    width: 30%;
-  }
-
-  .headerSettingUser > img{
-    height: 100%;
-
-  }
-
-  .headerSettingUser > p{
-    color: white;
-    font-size: 15px;
-  }
-
-
-
+<style lang="scss">
+  @import './HeaderLayout.scss';
 </style>
