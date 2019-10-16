2b3d3a0fdc10111fe3a5f4b845d792b1dd325e4a
squeen <shelltlove1@naver.com>
2019-10-17 01:35:58 +0900

    Add paper components
    개별 논문 컴포넌트를 추가함

--- your-paper-fe/src/pages/Main.vue
+++ your-paper-fe/src/pages/Main.vue
@@ -2,22 +2,28 @@
     <div id="mainOuterLayout">
       <header-layout id="headerLayoutRoute"></header-layout>
       <div id="mainContentLayout">
-        <main-option></main-option>
+        <div id="mainFilterLayout">
+          <main-option></main-option>
+        </div> <!--필터 레이아웃-->
+        <div id="mainListLayout">
+          <main-list></main-list>
+        </div> <!--글목록 레이아웃-->
       </div>
-
     </div>
 </template>
 
 <script>
 import headerLayout from '../components/Main/header/HeaderLayout'
 import mainOption from '../components/Main/contents/MainOption'
+import mainList from '../components/Main/contents/MainList'
 import '../components/BasicStyle/BasicColor.scss'
 
 export default {
   name: 'Main',
   components: {
     'header-layout': headerLayout,
-    'main-option': mainOption
+    'main-option': mainOption,
+    'main-list': mainList
   }
 }
 </script>
