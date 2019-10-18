b054cc7f70690482f649fdc5ecaca1c4ebcee67e
maple0110 <maple0110@naver.com>
2019-10-18 13:11:54 +0900

    폴더 계층 정리 및 폰트 주석 처리
    폰트로 인하여 나는 에러를 임시적으로 주석처리
    폴더의 계층을 맞춤

--- your-paper-fe/src/router.js
+++ your-paper-fe/src/router.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Landing from './pages/Landing.vue'
-
-import Main from './pages/Main.vue'
+// import Main from './pages/Main.vue'
 
 Vue.use(Router)
 
@@ -14,20 +13,12 @@ export default new Router({
       path: '/',
       name: 'landing',
       component: Landing
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './pages/About.vue')
-    },
-    {
-      path: '/main',
-      name: 'main',
-      component: Main
     }
+    // {
+    //   path: '/main',
+    //   name: 'main',
+    //   component: Main
+    // }
     // {
     //   path: '/',
     //   name: 'home',
