e3776eeaa0001e9100593042a30c855a45c09b4f
squeen <shelltlove1@naver.com>
2019-10-18 12:38:25 +0900

    Make My List component
    기존 Main.vue에 포함된 코드들이 MyList에 관한 코드들이어서 MyList컴포넌트를 생성해 모두 그 안으로 넣었음

--- your-paper-fe/src/pages/Main.vue
+++ your-paper-fe/src/pages/Main.vue
@@ -1,29 +1,17 @@
 <template>
-    <div id="mainOuterLayout">
-      <header-layout id="headerLayoutRoute"></header-layout>
-      <div id="mainContentLayout">
-        <div id="mainFilterLayout">
-          <main-option></main-option>
-        </div> <!--필터 레이아웃-->
-        <div id="mainListLayout">
-          <main-list></main-list>
-        </div> <!--글목록 레이아웃-->
-      </div>
+    <div>
+      <my-list id="myListOuterLayout"></my-list>
     </div>
 </template>
 
 <script>
-import headerLayout from '../components/Main/header/HeaderLayout'
-import mainOption from '../components/Main/contents/MainOption'
-import mainList from '../components/Main/contents/MainList'
 import '../components/BasicStyle/BasicColor.scss'
+import myList from '../components/Main/contents/MyList/MyListLayout'
 
 export default {
   name: 'Main',
   components: {
-    'header-layout': headerLayout,
-    'main-option': mainOption,
-    'main-list': mainList
+    'my-list': myList
   }
 }
 </script>
