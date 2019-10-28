04ca98a82306ace9129544da5919b6654a8122e1
maple0110 <maple0110@naver.com>
2019-10-28 15:07:18 +0900

    signup component 제작
    수정필요

--- your-paper-fe/src/components/5.landingBackground/landingBackground.vue
+++ your-paper-fe/src/components/5.landingBackground/landingBackground.vue
@@ -7,19 +7,40 @@
       <div class="logoImageContainer">
         <img class="logoImage" src="@/assets/images/paperLogo.png" alt="logoImage">
       </div>
-      <div class="descriptionContainer">
-        <p class="siteName">Your Paper</p>
-        <p class="siteDescription">Paper Management System</p>
+      <div class="leftDescriptionContainer">
+        <p class="leftSiteName">Your Paper</p>
+        <p class="leftSiteDescription">Paper Management System</p>
       </div>
     </div>
     <div class="rightContainer">
+      <div class="rightDescritionContainer">
+        <p class="rightSiteName">Your Paper</p>
+        <p class="rightSiteDescription">Paper Management System</p>
+      </div>
+      <div class="formContainer">
+        <login v-show="flag===1"></login>
+        <create v-show="flag===2"></create>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import login from '../5.landingBackground/login/loginComponent.vue'
+import create from '../5.landingBackground/create/createComponent.vue'
+
 export default {
-  name: 'landingBackground'
+
+  name: 'landingBackground',
+  components: {
+    login,
+    create
+  },
+  data () {
+    return {
+      flag: 2
+    }
+  }
 }
 </script>
 
---
+++ your-paper-fe/src/components/5.landingBackground/login/loginComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="loginWrapper">
+    <input class="formForId formElement" type="text" placeholder="ID">
+    <input class="formForPassword formElement" type="password" placeholder="Password">
+    <button class="buttonForLogin formElement" type="button">Login</button>
+    <button class="buttonForSignUp formElement" type="button">SignUp</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="scss">
+  @import './loginComponent.scss';
+</style>
