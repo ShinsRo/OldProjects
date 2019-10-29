7dc4ad78d5345001fde8918a335caa3e899cc64a
squeen <shelltlove1@naver.com>
2019-10-29 16:38:12 +0900

    Refactor components realated with header

--- your-paper-fe/src/pages/MainPage/Main.vue
+++ your-paper-fe/src/pages/MainPage/Main.vue
@@ -1,20 +1,21 @@
 <template>
     <div>
-      <!-- <header></header> -->
+      <headerComponent id="header"></headerComponent>
       <!-- <myList></myList> 내 노문 검색-->
       <!-- <paperStatics></paperStatics> 내 논문 통계-->
       <!-- <paperEdit></paperEdit> 내 논문 편집 -->
-      <my-list id="myListOuterLayout"></my-list>
+      <!--<my-list id="myListOuterLayout"></my-list>-->
     </div>
 </template>
 
 <script>
-import myList from '../components/Main/contents/MyList/MyListLayout'
-
+//import myList from '../components/Main/contents/MyList/MyListLayout'
+import headerComponent from '../../components/1.header/headerComponent.vue'
 export default {
   name: 'Main',
   components: {
-    'my-list': myList
+    //'my-list': myList
+    'headerComponent': headerComponent
   }
 }
 </script>
