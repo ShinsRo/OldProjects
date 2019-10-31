703e8b939dd97def86943d0425791b17fc4fe7c1
maple0110 <maple0110@naver.com>
2019-10-31 10:41:54 +0900

    axios plugin install
    login Button 클릭시 mainPage로 라우터 푸쉬

--- your-paper-fe/src/components/5.landingBackground/login/loginComponent.vue
+++ your-paper-fe/src/components/5.landingBackground/login/loginComponent.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="loginWrapper">
-    <input class="formForId formElement" type="text" placeholder="ID">
-    <input class="formForPassword formElement" type="password" placeholder="Password">
-    <button class="buttonForLogin formElement" type="button">Login</button>
+    <input class="formForId formElement" type="text" placeholder="ID" v-model="customer.id">
+    <input class="formForPassword formElement" type="password" placeholder="Password" v-model="customer.password">
+    <!-- <p class="inputErrorMessage">{{ wrongMessage.inputError }}</p> -->
+    <p class="loginErrorMessage">{{ wrongMessage.loginError}}</p>
+    <button class="buttonForLogin formElement" type="button" v-on:click="clickForLogin">Login</button>
     <button class="buttonForSignUp formElement" type="button" v-on:click="clickForSignUp">SignUp</button>
   </div>
 </template>
@@ -10,9 +12,28 @@
 <script>
 export default {
   name: 'login',
+  data () {
+    return {
+      customer : {
+        id : '',
+        password : ''
+      },
+      wrongMessage : {
+        inputError : '',
+        loginError : ''
+      }
+    }
+  },
   methods: {
     clickForSignUp () {
       this.$emit('changeFlag')
+    },
+    clickForLogin () {
+      if(this.customer.id !== '' && this.customer.password !== '') {
+        this.$router.push('/main')
+      } else {
+        this.wrongMessage.loginError = '아이디와 비밀번호를 확인해주세요'
+      }
     }
   }
 }
