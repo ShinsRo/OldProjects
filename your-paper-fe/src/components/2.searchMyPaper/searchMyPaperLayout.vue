8bb99375ff21fbe567654f85525a17f2cfb35ce0
squeen <shelltlove1@naver.com>
2019-11-01 12:17:03 +0900

    fix Header component  on Main Component

---
+++ your-paper-fe/src/components/2.searchMyPaper/searchMyPaperLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div id="mainContentLayout">
+      <div id="mainFilterLayout">
+        d
+        <!--<main-option></main-option>-->
+      </div> <!--필터 레이아웃-->
+      <div id="mainListLayout">
+        <!--<main-list></main-list>-->
+      </div> <!--글목록 레이아웃-->
+    </div>
+  </div>
+</template>
+
+<script>
+  //import mainOption from './MainOption'
+  //import mainList from './MainList'
+
+  export default {
+    name: 'MyListLayout',
+    components: {
+      'header-layout': headerLayout,
+      //'main-option': mainOption,
+      //'main-list': mainList,
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import './searchMyPaperLayout.scss';
+
+</style>
--- your-paper-fe/src/pages/MainPage/Main.vue
+++ your-paper-fe/src/pages/MainPage/Main.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
+
       <headerComponent id="header"></headerComponent>
+      <!--<searchMyPaperLayout id="searchMyPaperLayout"></searchMyPaperLayout>-->
       <!-- <myList></myList> 내 노문 검색-->
       <!-- <paperStatics></paperStatics> 내 논문 통계-->
       <!-- <paperEdit></paperEdit> 내 논문 편집 -->
@@ -11,11 +13,13 @@
 <script>
 //import myList from '../components/Main/contents/MyList/MyListLayout'
 import headerComponent from '../../components/1.header/headerComponent.vue'
+import searchMyPaperLayout from '../../components/2.searchMyPaper/searchMyPaperLayout.vue'
 export default {
   name: 'Main',
   components: {
-    //'my-list': myList
-    'headerComponent': headerComponent
+      //'my-list': myList
+      'headerComponent': headerComponent,
+    'searchMyPaperLayout' : searchMyPaperLayout
   }
 }
 </script>
