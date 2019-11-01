6d195bad628896c03513ae49881dd05ca106a4c3
squeen <shelltlove1@naver.com>
2019-11-01 14:58:00 +0900

    add viewMyPaperComponent on Main.vue

--- your-paper-fe/src/components/2.searchMyPaper/searchMyPaperLayout.vue
+++ your-paper-fe/src/components/2.searchMyPaper/searchMyPaperLayout.vue
@@ -7,19 +7,18 @@
     </div>
     <searchMyPaperWithOption id="searchMyPaperWithOption"></searchMyPaperWithOption>
     <!--필터 레이아웃-->
-      <div id="mainListLayout">
-        <!--<main-list></main-list>-->
-      </div> <!--글목록 레이아웃-->
+    <viewMyPaperComponent id="viewMyPaperComponent"></viewMyPaperComponent>
   </div> <!--MyPaper 전체 레이아웃-->
 </template>
 
 <script>
 import searchMyPaperWithOption from './searchMyPaperWithOption/searchMyPaperWithOptionComponent.vue'
-
+import viewMyPaperComponent from './viewMyPaper/viewMyPaperComponent.vue'
   export default {
     name: 'MyListLayout',
     components: {
-      'searchMyPaperWithOption' : searchMyPaperWithOption
+      'searchMyPaperWithOption' : searchMyPaperWithOption,
+      'viewMyPaperComponent' : viewMyPaperComponent
 
     }
   }
---
+++ your-paper-fe/src/components/2.searchMyPaper/viewMyPaper/viewMyPaperComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="viewMyPaperComponentLayout">
+    <div>
+      <div class="mainOptionFilter optionCheck">
+        <div class="mainOptionFilterContent">
+          <input class="check" type="checkbox"/>
+          <p class="text">
+            주 저자 여부
+          </p>
+        </div>
+        <div class="mainOptionFilterContent">
+          <input class="check" type="checkbox"/>
+          <p class="text">
+            피 인용수
+          </p>
+        </div>
+        <div class="mainOptionFilterContent">
+          <input class="check" type="checkbox"/>
+          <p class="text">
+            권, 호, 페이지
+          </p>
+        </div>
+        <div class="mainOptionFilterContent">
+          <input class="check" type="checkbox"/>
+          <p class="text">
+            상태 표시
+          </p>
+        </div>
+        <div class="mainOptionFilterContent">
+          <input class="check" type="checkbox"/>
+          <p class="text">
+            URL
+          </p>
+        </div>
+
+      </div>
+
+      <div class="mainOptionFilter" style="border-bottom: none; padding-left: 40px;">
+        <div class="mainOptionOrderName">
+          <p class="text">
+            최신 순
+          </p>
+        </div>
+        <div class="mainOptionOrderName">
+          <p class="text">
+            인용 순
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+    <paperDataComponent class="paperComponentLayout"></paperDataComponent>
+    <paperDataComponent class="paperComponentLayout"></paperDataComponent>
+    <paperDataComponent class="paperComponentLayout"></paperDataComponent>
+  </div>
+</template>
+
+<script>
+  import paperDataComponent from './paperData/paperDataComponent.vue'
+  export default {
+    name: 'MainList',
+    components: {
+      'paperDataComponent': paperDataComponent
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import './viewMyPaperComponent.scss';
+
+</style>
