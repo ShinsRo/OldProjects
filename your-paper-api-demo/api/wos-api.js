43b17173685b34d780a795d5c3e0806b205a33df
김승신 <siotman0728@gmail.com>
2019-11-12 16:48:05 +0900

    wos에서 검색하기 기능에 대한 데모 페이지
    1. 소트필드 추가
    2. 페이징 처리 추가
    3. 로딩 스텝 추가

--- your-paper-api-demo/api/wos-api.js
+++ your-paper-api-demo/api/wos-api.js
@@ -1,18 +1,21 @@
-const SERVER_URL = 'http://127.0.0.1:9400/';
-
 class WokSearchClient {
-    constructor() {
+    constructor(SERVER_URL) {
+        this.SERVER_URL     = SERVER_URL;
         this.clientId       = '';
         this.loading        = false;
+        this.step           = 0;
+        this.totalStep      = 1;
         this.timeSpanRegex  = /^\d{4}-\d{2}-\d{2}$/;
         this.records        = [];
         this.pageState = {
-            pageSize        : 50,
+            pageSize        : 10,
             currentPage     : 0,
             recordsFound    : 0,
             firstRecord     : 1,
+            pageList        : [],
             endPage         : 0,
         };
+        this.sortField = [];
         this.currentQueryId = 0;
         this.currentSid = 0;
     }
@@ -20,31 +23,40 @@ class WokSearchClient {
     getPageState() {
         return {...this.pageState};
     }
+    
+    getPageList() {
+        return this.pageState.pageList.slice(0);
+    }
 
     setLoading(loading) {
         this.loading = loading;
     }
 
     initQueryState(sid, queryId, recordsFound) {
-        const endPage = Math.ceil(recordsFound / this.pageState.pageSize);
-        const firstRecord = 1;
-        const currentPage = 1;
+        const endPage       = Math.ceil(recordsFound / this.pageState.pageSize);
+        const pageList      = [];
+        const firstRecord   = 1;
+        const currentPage   = 1;
+        
+        let i = 1;
+        while (i <= endPage) { pageList.push(i++); }
 
-        this.records    = Array(recordsFound + 1);
+        this.records    = [];
         this.currentSid = sid;
         this.currentQueryId = queryId;
-        this.pageState = {...this.pageState, recordsFound, firstRecord, currentPage, endPage};
+        this.sortField  = [];
+        this.pageState = {...this.pageState, recordsFound, firstRecord, currentPage, pageList, endPage};
     }
 
-    setRecords(firstRecord, origin) {
+    setRecords(origin) {
         origin.forEach((record, idx) => {
-            this.records[firstRecord + idx] = record;
+            this.records[idx] = record;
         });
     }
 
-    updateRecords(firstRecord, linksData) {
+    updateRecords(linksData) {
         linksData.forEach((record, idx) => {
-            this.records[firstRecord + idx]['lamrData'] = record;
+            this.records[idx]['lamrData'] = record;
         })
     }
 
@@ -55,7 +67,7 @@ class WokSearchClient {
     setPage(page) {
         const currentPage   = page;
         const count         = this.pageState.pageSize;
-        const firstRecord   = count * page + 1;
+        const firstRecord   = count * (page - 1) + 1;
         this.pageState = {...this.pageState, currentPage, firstRecord, count};
     }
 
@@ -67,17 +79,30 @@ class WokSearchClient {
             console.log("BuildUserQuery 인자 유효성 확인 완료");
         }
         
-        organizations = (organizations && Array.isArray(organizations) && organizations.length != 0) || '';
+        organizations = (Array.isArray(organizations) && organizations.length != 0 && organizations) || '';
+
         const organizationQuery = organizations && `AD=(${organizations.map(item => `(${item})`).join(" OR ")}) AND `;
         const userQuery = `${organizationQuery}${category}=(${query})`;
         console.log(userQuery);
         
         return userQuery;
     }
+    setSortField(sortBy, isAsc) {
+        if (sortBy === 'TC') isAsc = false;
+        if (!sortBy) {
+            this.sortField = [];
+            return;
+        }
+        const sortField = [{ 'name': sortBy, 'sort': (isAsc)? 'A':'D' }];
+        this.sortField = sortField;
+    }
 
-    search(userQuery, begin, end) {
-        this.loading = true;
-        if (!begin.match(this.timeSpanRegex) || !end.match(this.timeSpanRegex)) {
+    search(userQuery, begin, end, symbolicTimeSpan, noLAMR) {
+        this.step       = 0;
+        this.loading    = true;
+        this.noLAMR   = noLAMR;
+
+        if (!symbolicTimeSpan && !begin.match(this.timeSpanRegex) || !end.match(this.timeSpanRegex)) {
             console.error("WokSearchClient search의 인자가 유효하지 않습니다.");
             return;
         } else {
@@ -88,21 +113,32 @@ class WokSearchClient {
         const queryParameters = {
             'databaseId': 'WOS',
             'userQuery' : userQuery,
-            'timeSpan'  : {begin, end},
             'queryLanguage': 'en'
         };
+
+        if (symbolicTimeSpan) {
+            queryParameters['symbolicTimeSpan'] = symbolicTimeSpan;
+        } else {
+            queryParameters['timeSpan'] = { 'begin': begin, 'end': end };
+        }
+        
         const retrieveParameters = {
             'firstRecord'   : this.pageState.firstRecord, 
             'count'         : this.pageState.pageSize
         };
-        const data = {queryParameters, retrieveParameters};
+        if (this.sortField) retrieveParameters['sortField'] = this.sortField;
 
-        return axios.post(SERVER_URL + 'WokSearchService/search', data).then((response) => {
+        const data = {queryParameters, retrieveParameters};
+        console.log(data);
+        
+        return axios.post(this.SERVER_URL + 'WokSearchService/search', data).then((response) => {
+            this.step       = 1;
             const searchResult = response.data;
             console.log(searchResult);
             
             return searchResult;
         }).then(async (searchResult) => {
+            this.step       = 2;
             /* 3. 최초 검색 후 처리해야할 것들 */
             console.log('검색 내용에 대한 클라이언트 내부 수행 [단계 2/3] :');
 
@@ -112,27 +148,33 @@ class WokSearchClient {
             const sid           = searchResult['sid'];
 
             this.initQueryState(sid, queryId, recordsFound);
-            this.setRecords(1, searchResult['records']);
+            this.setRecords(searchResult['records']);
 
             // 검색 결과에 대해 페이지가 몇개인지 확인할 수 있음
             // console.log(this.getPageState());
             
             // 3-2. 링크를 받아올 논문들의 아이디들 추출
-            const uids = this.extractUidsFromRecords(searchResult['records']);
 
             // 링크 데이터 요청
-            await this.getLinksAndTimeCited(uids).then(response => {
-                console.log('링크 데이터 요청과 처리 [단계 3/3] :');
-                const linksData = response.data;
-                this.updateRecords(this.pageState.firstRecord, linksData);
-                this.setLoading(false);
-            });
-            
-            return this.records.slice(this.pageState.firstRecord, this.pageState.firstRecord + this.pageState.pageSize);
+            if (noLAMR) {
+                this.step = 3;
+                const uids = this.extractUidsFromRecords(searchResult['records']);
+
+                await this.getLinksAndTimeCited(uids).then(response => {
+                    this.step = 4;
+                    console.log('링크 데이터 요청과 처리 [단계 3/3] :');
+                    const linksData = response.data;
+                    this.updateRecords(linksData);
+                });
+            }
+            this.step = 5;
+            this.setLoading(false);
+            return searchResult;
         });
     }
 
     retreive(page) {
+        this.step = 0;
         this.setLoading(true);
         if (isNaN(page)) {
             console.error("WokSearchClient retrieve 인자가 유효하지 않습니다.");
@@ -146,6 +188,7 @@ class WokSearchClient {
             'firstRecord'   : this.pageState.firstRecord, 
             'count'         : this.pageState.pageSize
         };
+        if (this.sortField) retrieveParameters['sortField'] = this.sortField;
 
         const data = { 
             'sid': this.currentSid, 
@@ -153,22 +196,29 @@ class WokSearchClient {
             retrieveParameters 
         };
 
-        return axios.post(SERVER_URL + 'WokSearchService/retrieve', data).then(response => {
+        return axios.post(this.SERVER_URL + 'WokSearchService/retrieve', data).then(response => {
+            this.step = 1;
             const retreiveResult = response.data;
             return retreiveResult;
         }).then(async retreiveResult => {
-            const firstRecord = this.pageState.firstRecord;
-            this.setRecords(firstRecord, retreiveResult['records']);
-            const uids = this.extractUidsFromRecords(retreiveResult['records']);
-
+            this.step = 2;
+            this.setRecords(retreiveResult['records']);
+            
             // 링크 데이터 요청
-            await this.getLinksAndTimeCited(uids).then(response => {
-                console.log('링크 데이터 요청과 처리 [단계 3/3] :');
-                const linksData = response.data;
-                this.updateRecords(firstRecord, linksData);
-                this.setLoading(false);
-            });
-            return this.records.slice(this.pageState.firstRecord, this.pageState.firstRecord + this.pageState.pageSize);
+            if (this.noLAMR) {
+                this.step = 3;
+                const uids = this.extractUidsFromRecords(retreiveResult['records']);
+
+                await this.getLinksAndTimeCited(uids).then(response => {
+                    this.step = 4;
+                    console.log('링크 데이터 요청과 처리 [단계 3/3] :');
+                    const linksData = response.data;
+                    this.updateRecords(linksData);
+                    this.setLoading(false);
+                });
+            }
+            this.step = 5;
+            return retreiveResult;
         });
     }
 
@@ -187,6 +237,6 @@ class WokSearchClient {
             console.log("getLinksAndTimeCited 인자 유효성 확인 완료");
         };
         const data = {uids};
-        return axios.post(SERVER_URL + 'LamrService/lamrCorCollectionData', data);
+        return axios.post(this.SERVER_URL + 'LamrService/lamrCorCollectionData', data);
     }
 }
