35bbf5f7209daf584a6479164b11e648d110aeb9
squeen <shelltlove1@naver.com>
2019-12-09 11:12:11 +0900

    Binding statics data

--- your-paper-fe/src/components/6.paperStatics/allStatics/eachStatic/paperRatingComponent.vue
+++ your-paper-fe/src/components/6.paperStatics/allStatics/eachStatic/paperRatingComponent.vue
@@ -8,23 +8,37 @@
 export default {
   name: 'paperRatingComponent',
   mounted () {
-    var chart = c3.generate({
-    bindto: '#ratingContainer',
-    data: {
-        columns: [
-            ['SCI', 60],
-            ['SCIE', 30],
-            ['SCPIS', 30]
-        ],
-        type : 'donut',
-        onclick: function (d, i) { console.log('onclick', d, i); },
-        onmouseover: function (d, i) { console.log('onmouseover', d, i); },
-        onmouseout: function (d, i) { console.log('onmouseout', d, i); }
+
+  },
+  computed: {
+    getGrade () {
+      return this.$store.getters.NONE_GRADE_GETTER
     },
-    donut: {
-        title: '논문 등급 비율'
+  },
+  watch: {
+    getGrade () {
+      console.log( this.$store.getters.NONE_GRADE_GETTER)
+      var chart = c3.generate({
+
+        bindto: '#ratingContainer',
+        data: {
+          columns: [
+            ['SCI', this.$store.getters.SCI_GRADE_GETTER],
+            ['SCIE',this.$store.getters.SCIE_GRADE_GETTER],
+            ['SCPIS', this.$store.getters.SCPIS_GRADE_GETTER],
+            ['NONE', this.$store.getters.NONE_GRADE_GETTER],
+          ],
+          type : 'donut',
+          onclick: function (d, i) { console.log('onclick', d, i); },
+          onmouseover: function (d, i) { console.log('onmouseover', d, i);},
+          onmouseout: function (d, i) { console.log('onmouseout', d, i); }
+        },
+        donut: {
+          title: '논문 등급 비율'
+        }
+      });
+
     }
-    });
   }
 }
 </script>
--- your-paper-fe/src/components/6.paperStatics/paperStaticsLayout.vue
+++ your-paper-fe/src/components/6.paperStatics/paperStaticsLayout.vue
@@ -4,7 +4,7 @@
   <allPaperList id="allPaperList" :loading="loading" :paperData="paperData"></allPaperList>
   <div id="buttonWrap">
     <div class="button" > < </div>
-    <div v-for="i in endPage-1" class="page" v-on:click="page = i-1">{{i}}</div>
+    <div v-for="i in endPage-1" class="page" v-on:click="page = i">{{i}}</div>
     <div class="button"> > </div>
   </div>
 
@@ -24,6 +24,7 @@ export default {
   data(){
     return{
       page: 0,
+      pageFlag: 1,
       reprint: 'REPRINT',
       endPage: 1,
       loading: 0,
@@ -31,14 +32,19 @@ export default {
       paperData: [],
       count: 10,
       criteria: [
+        { field: this.$FIELD.TITLE, operation: this.$CRITERIA.LIKE, value: ' '},
         { field: this.$FIELD.AUTHOR_TYPE, operation: this.$CRITERIA.LIKE, value: 'REPRINT'},
-        ],
-      citedStack: 0
+        { field: this.$FIELD.AUTHOR_TYPE, operation: this.$CRITERIA.LIKE, value: 'GENERAL'},
+      ],
+      citedStack: 0,
+      orderBy: this.$FIELD.YEAR,
+      isComputed: 0
     }
   },
   mounted(){
     this.$store.dispatch('MEMBER_OBJECT_SET_ACTION')
-    this.$store.dispatch('SET_END_PAGE_ACTION', {count: this.count, criteria: this.criteria})
+    this.$store.dispatch('SET_SEARCH_FLAG_ACTION')
+    this.$store.dispatch('SET_END_PAGE_ACTION', {count: this.count, criteria: [this.criteria[0]]})
     //          0    1      2      3     4
     //         '행', 'UID', 'DOI', '제목', '링크',
     //          5        6          7      8
@@ -52,6 +58,9 @@ export default {
     isReadyToLoading(){
       return  this.$store.getters.END_PAGE_GETTER
     },
+    completeCompute(){
+      return this.$store.getters.GRADE_GETTER
+    },
     isLoading () {
       return this.$store.getters.MEMBER_PAPER_GETTER
     },
@@ -64,30 +73,33 @@ export default {
   },
   watch: {
     isReadyToLoading () {
-      this.endPage = this.$store.getters.END_PAGE_GETTER
-      this.$store.dispatch('ALL_PAPER_ACTION', {count: this.count , criteria: this.criteria})
-    },
-    isLoading () {
-        this.allPaperData = this.$store.getters.MEMBER_PAPER_GETTER
-    },
-    loadPaper () {
-      if (this.allPaperData === []) {
-        this.loading = -1
+      if(this.isComputed === 0){
+        this.endPage = this.$store.getters.END_PAGE_GETTER
+        this.$store.dispatch('COMPUTE_MY_PAPER_INFO_ACTION', {count: this.count , criteria: [this.criteria[0]]})
       }
       else {
-        this.paperData = this.allPaperData[this.page]
-        this.loading = 1
+        this.endPage = this.$store.getters.END_PAGE_GETTER
+        this.$store.dispatch('NEW_MEMBER_PAPER_ACTION', {count : this.count, orderBy: this.orderBy, criteria: [this.criteria[this.pageFlag]]})
       }
+    },
+    completeCompute(){
+      this.$store.dispatch('SET_END_PAGE_ACTION', {count: this.count, criteria: [this.criteria[this.pageFlag]]})
+      this.isComputed = 1
+    },
+    isLoading () {
+      this.paperData = this.$store.getters.MEMBER_PAPER_GETTER
       this.countCited()
+      this.loading = 1
+      console.log("paper Data",this.paperData)
     },
     paging () {
-      this.paperData = this.allPaperData[this.page]
+      this.$store.dispatch('MEMBER_PAPER_PAGING_ACTION', this.page)
     }
   },
   methods: {
     countCited () {
-      for (var i in this.allPaperData[0]) {
-        this.citedStack += this.allPaperData[0][i][5]
+      for (var i in this.paperData) {
+        this.citedStack += this.paperData[i][5]
       }
       if (this.citedStack !== 0) {
         this.$store.dispatch('CITE_COUNT_ACTION', this.citedStack)
